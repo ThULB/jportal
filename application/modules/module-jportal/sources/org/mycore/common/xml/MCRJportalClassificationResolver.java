28f04be5a87c886adb90387d63b1b1b059fe030d
Andreas Trappe <Andreas.Trappe@thulb.uni-jena.de>
2007-02-20 10:29:14 +0000

    read access faster and first try to use classi uri-resolver for editor

--- application/modules/module-jportal/sources/org/mycore/common/xml/MCRJportalClassificationResolver.java
+++ application/modules/module-jportal/sources/org/mycore/common/xml/MCRJportalClassificationResolver.java
@@ -3,6 +3,7 @@
  */
 package org.mycore.common.xml;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -10,6 +11,7 @@ import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
 import org.mycore.common.MCRCache;
 import org.mycore.common.MCRConfiguration;
 import org.mycore.common.MCRException;
@@ -36,20 +38,13 @@ public class MCRJportalClassificationResolver implements MCRResolverProvider {
 	}
 	
     private static class MCRJPortalClassification implements MCRResolver {
+    	
     	private static final Logger LOGGER = Logger.getLogger(MCRURIResolver.class);
-
-        //private static final Pattern EDITORFORMAT_PATTERN = Pattern.compile("(\\[)([^\\]]*)(\\])");
-
         private static final MCRConfiguration CONFIG = MCRConfiguration.instance();
-
         private static final String CONFIG_PREFIX = "MCR.UriResolver.";
-        
-        //private static final String FORMAT_CONFIG_PREFIX = CONFIG_PREFIX+"classification.format.";
-        
         private static MCRCache CLASS_CACHE;
-        
         private static long CACHE_INIT_TIME;
-
+        
         private static String URI_PREFIX = "jportalClassification";
         
         public MCRJPortalClassification(){
@@ -67,7 +62,9 @@ public class MCRJportalClassificationResolver implements MCRResolverProvider {
          * The alias is resolved by getting classification ID as value from MCRSession for key=alias 
          * 
          * Syntax:
-         * <code>jportalClassification:alias
+         * <code>jportalClassification:getClass:alias
+         * or
+         * <code>jportalClassification:getClassID:alias:xpathToBeFilled
          * 
          * @param uri
          *            URI in the syntax above
@@ -79,36 +76,74 @@ public class MCRJportalClassificationResolver implements MCRResolverProvider {
             
             if (!wellURI(uri)) 
             	throw new IllegalArgumentException("Invalid format of uri given to resolve jportalClassification:"+uri);
+            
             String classID = resolveAlias(uri);
             if (classID==null)
             	throw new MCRException("Could not resolve given alias "+uri+" into MCRClassificationID");
             String classiURI = "classification:editor:-1:children:"+classID;
-            Element returns;
-            if (CONFIG.getSystemLastModified() > CACHE_INIT_TIME){
-                initCache();
-                returns = MCRURIResolver.instance().resolve(classiURI);
-                CLASS_CACHE.put(classiURI,returns);
-            } else {
-                returns=(Element)CLASS_CACHE.get(uri);
-                if (returns==null){
-                    returns = MCRURIResolver.instance().resolve(classiURI);
-                    CLASS_CACHE.put(classiURI,returns);
-                }
-            }
-            return returns;
+            
+            String[] parameters = uri.split(":");
+            Element returnXML;
+            if (parameters[1].equals("getClassID")) {
+            	returnXML = new Element("hidden");
+            	returnXML.setAttribute("var", parameters[3]);
+            	returnXML.setAttribute("default",classID);
+            	LOGGER.debug("1########################");
+            	LOGGER.debug("get ClassID, return=");
+            	XMLOutputter out = new XMLOutputter();
+            	try {
+					out.output(returnXML, System.out);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            	LOGGER.debug("2#############################################################################################");            	
+            	
+			} else {
+				if (CONFIG.getSystemLastModified() > CACHE_INIT_TIME){
+	                initCache();
+	                returnXML = MCRURIResolver.instance().resolve(classiURI);
+	                CLASS_CACHE.put(classiURI,returnXML);
+	            } else {
+	                returnXML=(Element)CLASS_CACHE.get(uri);
+	                if (returnXML==null){
+	                    returnXML = MCRURIResolver.instance().resolve(classiURI);
+	                    CLASS_CACHE.put(classiURI,returnXML);
+	                }
+	            }
+            	LOGGER.debug("1#############################################################################################");
+            	LOGGER.debug("get Class");
+            	LOGGER.debug("2#############################################################################################"); 
+			}
+            if (parameters[1].equals("getClassID")) {
+            	LOGGER.debug("1########################");
+            	LOGGER.debug("das kommt raus =");
+            	XMLOutputter out = new XMLOutputter();
+            	try {
+    				out.output(returnXML, System.out);
+    			} catch (IOException e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			}
+            	LOGGER.debug("2#############################################################################################");  
+			}
+ 
+            return returnXML;
         }
 
 		private String resolveAlias(String uri) {
 			String[] parameters = uri.split(":");
             MCRSession session = MCRSessionMgr.getCurrentSession();
-            String alias = "XSL."+URI_PREFIX+"."+parameters[1];
+            String alias = "XSL."+URI_PREFIX+"."+parameters[2];
             String classID = (String) session.get(alias);
             return classID;
 		}
 
 		private boolean wellURI(String uri) {
 			String[] parameters = uri.split(":");
-            if (parameters.length==2 && parameters[0].equals(URI_PREFIX) && !parameters[1].equals("")){
+            if ( parameters[0].equals(URI_PREFIX) 
+            		&& ((parameters[1].equals("getClass") && !parameters[2].equals("") && parameters.length==3) 
+            			|| (parameters[1].equals("getClassID") && !parameters[2].equals("") && !parameters[3].equals("") && parameters.length==4))){
                 return true;
             }
             return false;
