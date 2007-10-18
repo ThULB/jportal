f555e32c3d52202207f9e50198422dd6ce1e0122
Andreas Trappe <Andreas.Trappe@thulb.uni-jena.de>
2007-10-18 07:56:33 +0000

    little refactorings

--- application/modules/module-jportal/sources/org/mycore/common/xml/MCRJPortalURIGetClassID.java
+++ application/modules/module-jportal/sources/org/mycore/common/xml/MCRJPortalURIGetClassID.java
@@ -12,7 +12,7 @@ public class MCRJPortalURIGetClassID implements MCRURIResolver.MCRResolver {
     private static final Logger LOGGER = Logger.getLogger(MCRJPortalURIGetClassID.class);
 
     private static String URI = "jportal_getClassID";
-    
+
     private static final String INCLUDE_TAG_RETURN_VALUE = "classification:editor[textcounter]:2:children:";
 
     /**
@@ -35,9 +35,9 @@ public class MCRJPortalURIGetClassID implements MCRURIResolver.MCRResolver {
      */
     public Element resolveElement(String uri) {
         LOGGER.debug("start resolving " + uri);
-        
-        if (!wellURI(uri).equals("true"))
-           throw new IllegalArgumentException("Invalid format of uri given to resolve " + URI + "=" + uri + " *** error="+wellURI(uri));
+
+        if (!wellURI(uri))
+            throw new IllegalArgumentException("Invalid format of uri given to resolve " + URI + "=" + uri);
 
         // get journal ID
         String journalID = MCRJPortalURIGetJournalID.getID();
@@ -47,32 +47,28 @@ public class MCRJPortalURIGetClassID implements MCRURIResolver.MCRResolver {
         String classID = getClassID(journalID, params[1]);
         if (classID == null)
             throw new MCRException("Could not resolve given alias " + uri + " into MCRClassificationID");
-        
+
         // answer xml
         Element returnXML = new Element("dummyRoot");
         if (returnIncludeTag(uri)) {
             String uriVal = INCLUDE_TAG_RETURN_VALUE + classID;
             returnXML.addContent(new Element("include").setAttribute("cacheable", "false").setAttribute("uri", uriVal));
-        } else 
+        } else
             returnXML.addContent(new Element("hidden").setAttribute("var", params[2]).setAttribute("default", classID));
-        
+
         return returnXML;
     }
 
     public static String getClassID(String journalID, String XPathWhereToFindClassIDInJournalXML) {
 
         // TODO: use cache
-        LOGGER.debug("#############################################");
         LOGGER.debug("getClassID => journalID=" + journalID);
-        LOGGER.debug("#############################################");
 
         Document journalXML = MCRXMLTableManager.instance().readDocument(new MCRObjectID(journalID));
         int sepPos = XPathWhereToFindClassIDInJournalXML.indexOf("/");
         String tag1 = XPathWhereToFindClassIDInJournalXML.substring(0, sepPos);
         String tag2 = XPathWhereToFindClassIDInJournalXML.substring(sepPos + 1, XPathWhereToFindClassIDInJournalXML.length());
-        LOGGER.debug("#############################################");
         LOGGER.debug("MCRJPortalURIGetClassID: ermittelte tags -> tag1=" + tag1 + " tag2=" + tag2);
-        LOGGER.debug("#############################################");
         Element classID_root = journalXML.getRootElement().getChild("metadata").getChild(tag1);
         String classID = null;
         if (classID_root != null && classID_root.getChild(tag2) != null)
@@ -80,26 +76,26 @@ public class MCRJPortalURIGetClassID implements MCRURIResolver.MCRResolver {
         return classID;
     }
 
-    private String wellURI(String uri) {
+    private boolean wellURI(String uri) {
         String[] parameters = uri.split(":");
         int numOfArgs = parameters.length;
         // number of given arguments correct ?
         if (numOfArgs < 3 || numOfArgs > 4)
-            return "false:1";
+            return false;
         // right uri
         if (!parameters[0].equals(URI))
-            return "false:2";
+            return false;
         // params are not empty ?
         if (numOfArgs == 3 && (parameters[1].equals("") || parameters[2].equals("")))
-            return "false:3";
+            return false;
         if (numOfArgs == 4 && (parameters[1].equals("") || parameters[2].equals("") || parameters[3].equals("")) && !returnIncludeTag(uri))
-            return "false:4";
-        return "true";
+            return false;
+        return true;
     }
 
     private boolean returnIncludeTag(String uri) {
         String[] parameters = uri.split(":");
-        if (parameters.length==4 && parameters[3].equals("getInclude"))
+        if (parameters.length == 4 && parameters[3].equals("getInclude"))
             return true;
         else
             return false;
