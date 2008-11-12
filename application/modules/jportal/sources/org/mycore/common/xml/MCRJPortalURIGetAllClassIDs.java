fca4dd5979d0b99168668c677b29e9c2d195a7b5
Andreas Trappe <Andreas.Trappe@thulb.uni-jena.de>
2008-11-12 11:45:42 +0000

    bug fix: if label can't be resolved, return empty one.

--- application/modules/jportal/sources/org/mycore/common/xml/MCRJPortalURIGetAllClassIDs.java
+++ application/modules/jportal/sources/org/mycore/common/xml/MCRJPortalURIGetAllClassIDs.java
@@ -26,9 +26,8 @@ public class MCRJPortalURIGetAllClassIDs implements MCRURIResolver.MCRResolver {
      * 
      * Syntax: jportal_getAllClassIDs
      * 
-     * @return <dummyRoot> <list type="dropdown"> <item value="{classification
-     *         id}"> <label xml:lang="de">{classification description}</label>
-     *         </item> </list> </dummyRoot>
+     * @return <dummyRoot> <list type="dropdown"> <item value="{classification id}"> <label xml:lang="de">{classification description}</label> </item> </list>
+     *         </dummyRoot>
      * 
      */
     public Element resolveElement(String uri) {
@@ -37,22 +36,34 @@ public class MCRJPortalURIGetAllClassIDs implements MCRURIResolver.MCRResolver {
         if (!wellURI(uri))
             throw new IllegalArgumentException("Invalid format of uri given to resolve " + URI + "=" + uri);
 
-        Element returnXML = new Element("dummyRoot");
-        String currentLang = MCRSessionMgr.getCurrentSession().getCurrentLanguage();
-        MCRCategoryDAO categDoa = MCRCategoryDAOFactory.getInstance();
-        Iterator<MCRCategoryID> ci = categDoa.getRootCategoryIDs().iterator();
-        while (ci.hasNext()) {
-            MCRCategoryID cid = (MCRCategoryID) ci.next();
-            String classID = cid.getRootID();
-            String descr = categDoa.getRootCategory(cid, 0).getLabels().get(currentLang).getText();
-            Element item = new Element("item").setAttribute("value", classID);
-            Element label = new Element("label").setAttribute("lang", "de", Namespace.XML_NAMESPACE);
-            label.setText(descr + " (" + classID + ")");
-            item.addContent(label);
-            returnXML.addContent(item);
+        try {
+            Element returnXML = new Element("dummyRoot");
+            String currentLang = MCRSessionMgr.getCurrentSession().getCurrentLanguage();
+            MCRCategoryDAO categDoa = MCRCategoryDAOFactory.getInstance();
+            Iterator<MCRCategoryID> ci = categDoa.getRootCategoryIDs().iterator();
+            while (ci.hasNext()) {
+
+                MCRCategoryID cid = (MCRCategoryID) ci.next();
+                String classID = cid.getRootID();
+                String descr = "";
+                if ((null != categDoa.getRootCategory(cid, 0).getLabels()) && (null != categDoa.getRootCategory(cid, 0).getLabels().get(currentLang))
+                                && null != categDoa.getRootCategory(cid, 0).getLabels().get(currentLang).getText()) {
+                    descr = categDoa.getRootCategory(cid, 0).getLabels().get(currentLang).getText();
+                }
+                // String descr = categDoa.getRootCategory(cid, 0).getLabels().get(currentLang).getText();
+                Element item = new Element("item").setAttribute("value", classID);
+                Element label = new Element("label").setAttribute("lang", "de", Namespace.XML_NAMESPACE);
+                label.setText(descr + " (" + classID + ")");
+                item.addContent(label);
+                returnXML.addContent(item);
+            }
+            return returnXML;
+        } catch (NullPointerException e) {
+            LOGGER.debug("Exeption occured: " + e);
+            throw e;
+
         }
-        
-        return returnXML;
+
     }
 
     private boolean wellURI(String uri) {
