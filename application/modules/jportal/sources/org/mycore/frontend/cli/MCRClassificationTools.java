9a853e8173d56b37dd38606425607fa952b551ff
Huu Chi Vu <chiliego.github@gmail.com>
2009-02-12 16:58:46 +0000

    add cli command for repair positionInParent

--- application/modules/jportal/sources/org/mycore/frontend/cli/MCRClassificationTools.java
+++ application/modules/jportal/sources/org/mycore/frontend/cli/MCRClassificationTools.java
@@ -18,10 +18,13 @@ public class MCRClassificationTools extends MCRAbstractCommands {
 
         com = new MCRCommand("repair category with empty labels", "org.mycore.frontend.cli.MCRClassificationTools.repairEmptyLabels", "");
         command.add(com);
+
+        com = new MCRCommand("repair position in parent", "org.mycore.frontend.cli.MCRClassificationTools.repairPositionInParent", "");
+        command.add(com);
     }
 
     // - get category without labels via SQL-query
-    // - set category ID as category label 
+    // - set category ID as category label
     public static void repairEmptyLabels() {
         Session session = MCRHIBConnection.instance().getSession();
         String sqlQuery = "select cat.classid,cat.categid from mcrcategory cat left outer join mcrcategorylabels label on cat.internalid = label.category where label.text is null";
@@ -33,10 +36,50 @@ public class MCRClassificationTools extends MCRAbstractCommands {
             String categIDString = (String) arrayOfResults[1];
 
             MCRCategoryID mcrCategID = new MCRCategoryID(classIDString, categIDString);
-            MCRLabel mcrCategLabel = new MCRLabel("de",categIDString,"");
+            MCRLabel mcrCategLabel = new MCRLabel("de", categIDString, "");
             MCRCategoryDAOFactory.getInstance().setLabel(mcrCategID, mcrCategLabel);
             LOGGER.info("fixing category with class ID \"" + classIDString + "\" and category ID \"" + categIDString + "\"");
         }
         LOGGER.info("Fixing category labels completed!");
+    }
+
+    public static void repairPositionInParent() {
+        Session session = MCRHIBConnection.instance().getSession();
+        // this SQL-query find missing numbers in positioninparent
+        String sqlQuery = "select parentid, min(cat1.positioninparent+1) from MCRCATEGORY cat1 " + "where cat1.parentid is not null and not exists"
+                + "(select 1 from MCRCATEGORY cat2 " + "where cat2.parentid=cat1.parentid and cat2.positioninparent=(cat1.positioninparent+1))"
+                + "and cat1.positioninparent not in " + "(select max(cat3.positioninparent) from MCRCATEGORY cat3 "
+                + "where cat3.parentid=cat1.parentid) group by cat1.parentid";
+
+        while (true) {
+            List<Object[]> parentWithErrorsList = session.createSQLQuery(sqlQuery).list();
+
+            if (parentWithErrorsList.isEmpty()) {
+                break;
+            }
+
+            for (Object[] parentWithErrors : parentWithErrorsList) {
+                Number parentID = (Number) parentWithErrors[0];
+                Number firstErrorPositionInParent = (Number) parentWithErrors[1];
+                LOGGER.info("Category " + parentID + " has the missing position " + firstErrorPositionInParent + " ...");
+                repairCategory(parentID, firstErrorPositionInParent);
+                LOGGER.info("Fixed position " + firstErrorPositionInParent + " for category " + parentID + ".");
+            }
+        }
+        LOGGER.info("Repair position in parent finished!");
+    }
+
+    private static void repairCategory(Number parentID, Number firstErrorPositionInParent) {
+        Session session = MCRHIBConnection.instance().getSession();
+        // the query decrease the position in parent with a rate.
+        // eg. posInParent: 0 1 2 5 6 7
+        // at 3 the position get faulty, 5 is the min. of the position greather
+        // 3
+        // so the reate is 5-3 = 2
+        String sqlQuery = "update MCRCATEGORY set positioninparent=(" + "positioninparent -" + "(select min(positioninparent) from MCRCATEGORY where parentid="
+                + parentID + " and positioninparent > " + firstErrorPositionInParent + ")+" + firstErrorPositionInParent + ") where parentid=" + parentID
+                + " and positioninparent > " + firstErrorPositionInParent;
+
+        session.createSQLQuery(sqlQuery).executeUpdate();
     }
 }
