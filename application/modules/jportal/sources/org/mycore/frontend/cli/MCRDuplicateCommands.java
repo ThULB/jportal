171e89114b2745b6f09159b31e3315271d62b52a
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2009-04-09 13:31:17 +0000

    - increased speed for generating a redundancy map - updated merge command for some special cases

--- application/modules/jportal/sources/org/mycore/frontend/cli/MCRDuplicateCommands.java
+++ application/modules/jportal/sources/org/mycore/frontend/cli/MCRDuplicateCommands.java
@@ -4,13 +4,14 @@ public class MCRDuplicateCommands extends MCRAbstractCommands {
 
     public MCRDuplicateCommands() {
         super();
-        MCRCommand findDuplicates = new MCRCommand("find doublets for type: {0}", "org.mycore.frontend.cli.command.MCRFindDuplicates.findDuplicates String", "creates the duplicates file from the previous generated checkForDuplicates-file");
-        command.add(findDuplicates);
+        
+        MCRCommand generateCommand = new MCRCommand("generate redundancy map for type {0}", "org.mycore.frontend.cli.command.MCRGenerateRedundancyMap.generate String", "generates a redundancy map for a specific type.");
+        command.add(generateCommand);
 
         MCRCommand cleanUpRed = new MCRCommand("clean up redundancy in database for type: {0}", "org.mycore.frontend.cli.command.MCRCleanUpRedundancyInDB.cleanUp String", "cleans up the redundancy in the database of a specific type.");
         command.add(cleanUpRed);
 
-        MCRCommand merge = new MCRCommand("merge file {1} of type {0} with redundancy map", "org.mycore.frontend.cli.command.MCRMergeAndreasRedundancyMap.merge String String", "");
+        MCRCommand merge = new MCRCommand("merge file {1} of type {0} with redundancy map", "org.mycore.frontend.cli.command.MCRMergeOldRedundancyMap.merge String String", "");
         command.add(merge);
 //        MCRCommand generatePersons = new MCRCommand("generate persons between {0} and {1}", "org.mycore.frontend.cli.command.MCRGenerateObjects.generatePersons int int", "generates persons between start and stop");
 //        command.add(generatePersons);
@@ -18,12 +19,6 @@ public class MCRDuplicateCommands extends MCRAbstractCommands {
 //        MCRCommand generateJpInst = new MCRCommand("generate institutions between {0} and {1}", "org.mycore.frontend.cli.command.MCRGenerateObjects.generateInstitutions int int", "generates institutions between start and stop");
 //        command.add(generateJpInst);
 
-        MCRCommand prInternalCheckForDup = new  MCRCommand("internal create checkForDuplicates.xml for type: {0}", "org.mycore.frontend.cli.command.MCRCheckForDuplicates.createCheckForDuplicatesFile String", "creates a complete xml-file for the specified type");
-        command.add(prInternalCheckForDup);
-
-        MCRCommand prInternalCreateRedundancy = new MCRCommand("internal create redundancy.xml for type: {0}", "org.mycore.frontend.cli.command.MCRCreateRedundancyMap.internalCreateRedundancyMap String", "creates the duplicates file from the previous generated checkForDuplicates-file");
-        command.add(prInternalCreateRedundancy);
-        
         MCRCommand prInternalProcessRedundancyObjects = new MCRCommand("internal process redundancy object {0}", "org.mycore.frontend.cli.command.MCRCleanUpRedundancyInDB.processRedundancyObject String", "");
         command.add(prInternalProcessRedundancyObjects);
 
