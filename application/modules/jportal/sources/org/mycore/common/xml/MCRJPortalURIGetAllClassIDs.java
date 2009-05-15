c3433f71ae99435614d3a3e99c01f170a976fcb4
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2009-05-15 15:35:01 +0000

    implemented a cache for getAllCategoryIDs + fixed a minor bug in mycore

--- application/modules/jportal/sources/org/mycore/common/xml/MCRJPortalURIGetAllClassIDs.java
+++ application/modules/jportal/sources/org/mycore/common/xml/MCRJPortalURIGetAllClassIDs.java
@@ -1,10 +1,13 @@
 package org.mycore.common.xml;
 
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.jdom.Element;
 import org.jdom.Namespace;
+import org.mycore.common.MCRCache;
+import org.mycore.datamodel.classifications2.MCRCategory;
 import org.mycore.datamodel.classifications2.MCRCategoryDAO;
 import org.mycore.datamodel.classifications2.MCRCategoryDAOFactory;
 import org.mycore.datamodel.classifications2.MCRCategoryID;
@@ -14,6 +17,10 @@ public class MCRJPortalURIGetAllClassIDs implements MCRURIResolver.MCRResolver {
     private static final Logger LOGGER = Logger.getLogger(MCRJPortalURIGetAllClassIDs.class);
 
     private static String URI = "jportal_getALLClassIDs:";
+
+    private Element returnXML = null;
+
+    private List<MCRCategoryID> categoryRootIDs = null;
 
     /**
      * 
@@ -29,31 +36,37 @@ public class MCRJPortalURIGetAllClassIDs implements MCRURIResolver.MCRResolver {
         if (!wellURI(uri))
             throw new IllegalArgumentException("Invalid format of uri given to resolve " + URI + "=" + uri);
 
-        try {
-            Element returnXML = new Element("dummyRoot");
-            MCRCategoryDAO categDoa = MCRCategoryDAOFactory.getInstance();
-            Iterator<MCRCategoryID> ci = categDoa.getRootCategoryIDs().iterator();
-            while (ci.hasNext()) {
-
-                MCRCategoryID cid = (MCRCategoryID) ci.next();
-                String classID = cid.getRootID();
-                String descr = "";
-                if ((null != categDoa.getRootCategory(cid, 0).getLabels())) {
-                    descr = categDoa.getRootCategory(cid, 0).getCurrentLabel().getText();
-                }
-                Element item = new Element("item").setAttribute("value", classID);
-                Element label = new Element("label").setAttribute("lang", "de", Namespace.XML_NAMESPACE);
-                label.setText(descr + " (" + classID + ")");
-                item.addContent(label);
-                returnXML.addContent(item);
-            }
-            return returnXML;
-        } catch (NullPointerException e) {
-            LOGGER.debug("Exeption occured: " + e);
-            throw e;
-
+        MCRCategoryDAO categDoa = MCRCategoryDAOFactory.getInstance();
+        List<MCRCategoryID> currentRootIds = categDoa.getRootCategoryIDs();
+        // if the current categorys differs from the previous generated,
+        // then create an updated xml tree
+        if(returnXML == null ||
+           categoryRootIDs == null ||
+           !currentRootIds.containsAll(categoryRootIDs)) {
+            categoryRootIDs = currentRootIds;
+            returnXML = createXML(categDoa, categoryRootIDs);
         }
+        return returnXML;
+    }
 
+    private Element createXML(MCRCategoryDAO categDoa, List<MCRCategoryID> ids) {
+        Element returnXML = new Element("dummyRoot");
+        Iterator<MCRCategoryID> ci = ids.iterator();
+        while (ci.hasNext()) {
+            MCRCategoryID cid = (MCRCategoryID) ci.next();
+            String classID = cid.getRootID();
+            String descr = "";
+            MCRCategory rootCat = categDoa.getRootCategory(cid, 0);
+            if ((null != rootCat.getLabels())) {
+                descr = rootCat.getCurrentLabel().getText();
+            }
+            Element item = new Element("item").setAttribute("value", classID);
+            Element label = new Element("label").setAttribute("lang", "de", Namespace.XML_NAMESPACE);
+            label.setText(descr + " (" + classID + ")");
+            item.addContent(label);
+            returnXML.addContent(item);
+        }
+        return returnXML;
     }
 
     private boolean wellURI(String uri) {
