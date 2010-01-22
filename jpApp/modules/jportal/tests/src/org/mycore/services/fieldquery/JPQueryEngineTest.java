7175c6da182494e4f22301d4cc3788b3e7d8a345
Huu Chi Vu <chiliego.github@gmail.com>
2010-01-22 09:36:01 +0000

    update Test

--- jpApp/modules/jportal/tests/src/org/mycore/services/fieldquery/JPQueryEngineTest.java
+++ jpApp/modules/jportal/tests/src/org/mycore/services/fieldquery/JPQueryEngineTest.java
@@ -1,15 +1,50 @@
 package org.mycore.services.fieldquery;
 
-import junit.framework.TestCase;
-
-import org.jdom.Element;
+import org.mycore.common.MCRConfiguration;
+import org.mycore.common.MCRTestCase;
 import org.mycore.parsers.bool.MCRCondition;
 
-public class JPQueryEngineTest extends TestCase {
+public class JPQueryEngineTest extends MCRTestCase {
+    private static final String SPLITTED_QUERY_STRING = "(objectType = \"foo\") AND (objectType = \"doo\")";
+    private static final String QUERY_STRING = "objectType = \"foo doo\"";
+
+    @Override
+    protected void setUp() throws Exception {
+        MCRConfiguration.instance().set("MCR.IndexBrowser.jpperson_sub.Searchfield", "objectType");
+        super.setUp();
+    }
+    
     public void testSplitQuery() throws Exception {
-        MCRQueryParser mcrQueryParser = new MCRQueryParser();
-        MCRCondition condition = mcrQueryParser.parse("(objectType = \"foo\")");
+        MCRQuery splittedQuery = splitQuery(createQuery(QUERY_STRING));
         
+        assertEquals(SPLITTED_QUERY_STRING, splittedQuery.getCondition().toString());
+    }
+
+    private MCRQuery splitQuery(MCRQuery query) {
+        JPQueryEngine queryEngine = new JPQueryEngine();
+        MCRQuery splittedQuery = queryEngine.splitCondition(query);
+        return splittedQuery;
+    }
+
+    private MCRQuery createQuery(String queryString) {
+        MCRQueryParser mcrQueryParser = new MCRQueryParser();
+        MCRCondition condition = MCRQueryParser.normalizeCondition(mcrQueryParser.parse(queryString));
         assertNotNull(condition);
+        
+        MCRQuery query = new MCRQuery(condition);
+        return query;
+    }
+    
+    public void testSplitQueryWithoutMatchingSearchfield() throws Exception {
+        MCRConfiguration.instance().set("MCR.IndexBrowser.jpperson_sub.Searchfield", "whatever");
+        MCRQuery splittedQuery = splitQuery(createQuery(QUERY_STRING));
+        
+        assertEquals(QUERY_STRING, splittedQuery.getCondition().toString());
+    }
+    
+    public void testSplitQueryNull() throws Exception {
+        MCRQuery splittedQuery = splitQuery(null);
+        
+        assertNull(splittedQuery);
     }
 }
