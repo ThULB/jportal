0ae0dad8b27a80277d3193caaf593bba5d78b1cd
Huu Chi Vu <chiliego.github@gmail.com>
2010-09-07 20:59:45 +0000

    start integrating JAX-SR

--- jpApp/modules/jportal/sources/org/mycore/resources/CalenderResource.java
+++ jpApp/modules/jportal/sources/org/mycore/resources/CalenderResource.java
@@ -2,13 +2,21 @@ package org.mycore.resources;
 
 import javax.ws.rs.Path;
 
+import org.jdom.Document;
+import org.mycore.services.fieldquery.MCRHit;
+import org.mycore.services.fieldquery.MCRQuery;
+import org.mycore.services.fieldquery.MCRQueryManager;
+import org.mycore.services.fieldquery.MCRResults;
 import org.mycore.services.fieldquery.MCRSearcherFactory;
 
 @Path("/calendar")
 public class CalenderResource {
 
     public Object calendar(){
-//        MCRSea
+        Document doc = null;
+        MCRQuery query = MCRQuery.parseXML(doc);
+        MCRResults results = MCRQueryManager.search(query);
+        //TODO: Jaxb wrapper for MCRResults
         return null;
     }
 }
