8380152f7219d4c15fd0408cb1ff788c5b81e330
Huu Chi Vu <chiliego.github@gmail.com>
2010-09-07 13:29:29 +0000

    integrate jersey/JSR into ZS

---
+++ jpApp/modules/jportal/sources/org/mycore/resources/CalenderResource.java
@@ -0,0 +1,14 @@
+package org.mycore.resources;
+
+import javax.ws.rs.Path;
+
+import org.mycore.services.fieldquery.MCRSearcherFactory;
+
+@Path("/calendar")
+public class CalenderResource {
+
+    public Object calendar(){
+//        MCRSea
+        return null;
+    }
+}
