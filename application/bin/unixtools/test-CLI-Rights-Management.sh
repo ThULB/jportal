d39bf201a68cdb7d38a41e5e7418db4cddce2dc8
Huu Chi Vu <chiliego.github@gmail.com>
2010-10-26 15:25:21 +0000

    start migrating to maven module

--- application/bin/unixtools/test-CLI-Rights-Management.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-mkdir -p $DOCPORTAL_HOME/testResults
-$DOCPORTAL_HOME/build/bin/mycore.sh list all permissions
-$DOCPORTAL_HOME/build/bin/mycore.sh export all permissions to file $DOCPORTAL_HOME/testResults/permission.xml
