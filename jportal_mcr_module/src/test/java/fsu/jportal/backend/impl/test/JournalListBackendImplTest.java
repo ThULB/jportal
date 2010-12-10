aa2631de5fa2757cd2a9546face61f045e44fcfc
Huu Chi Vu <chiliego.github@gmail.com>
2010-12-10 14:54:00 +0000

    Implement Journallist backend

---
+++ jportal_mcr_module/src/test/java/fsu/jportal/backend/impl/test/JournalListBackendImplTest.java
@@ -0,0 +1,21 @@
+package fsu.jportal.backend.impl.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mycore.datamodel.ifs2.MCRDirectory;
+import org.mycore.datamodel.ifs2.MCRFile;
+import org.mycore.datamodel.ifs2.MCRFileCollection;
+import org.mycore.datamodel.ifs2.MCRFileStore;
+import org.mycore.datamodel.ifs2.MCRNode;
+
+public class JournalListBackendImplTest {
+    @Test
+    public void getList() throws Exception {
+        MCRNode node;
+        MCRFileStore store;
+        MCRFileCollection coll;
+        MCRDirectory dir;
+        MCRFile journalListFile;
+    }
+}
