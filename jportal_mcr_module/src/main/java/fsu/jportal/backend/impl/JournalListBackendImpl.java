0814dbb30848140d8041d5cb4fd88a7385722d81
Huu Chi Vu <chiliego.github@gmail.com>
2010-12-20 15:11:41 +0000

    TODO journallist backend again

--- jportal_mcr_module/src/main/java/fsu/jportal/backend/impl/JournalListBackendImpl.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/backend/impl/JournalListBackendImpl.java
@@ -1,9 +1,21 @@
 package fsu.jportal.backend.impl;
 
+import org.mycore.datamodel.ifs2.MCRFileStore;
+
 import fsu.jportal.backend.api.JournalListBackend;
 import fsu.jportal.jaxb.JournalList;
 
 public class JournalListBackendImpl implements JournalListBackend{
+    
+    private MCRFileStore store;
+
+    public JournalListBackendImpl() {
+        setStore(new MCRFileStore());
+    }
+
+    public JournalListBackendImpl(MCRFileStore store) {
+        this.setStore(store);
+    }
 
     @Override
     public JournalList getList(String type) {
@@ -15,6 +27,14 @@ public class JournalListBackendImpl implements JournalListBackend{
     public JournalList createList(String type) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private void setStore(MCRFileStore store) {
+        this.store = store;
+    }
+
+    private MCRFileStore getStore() {
+        return store;
     }
 
 }
--- jportal_mcr_module/src/test/java/fsu/jportal/backend/impl/test/JournalListBackendImplTest.java
+++ jportal_mcr_module/src/test/java/fsu/jportal/backend/impl/test/JournalListBackendImplTest.java
@@ -3,21 +3,30 @@ package fsu.jportal.backend.impl.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.mycore.datamodel.ifs2.MCRDirectory;
+import org.mycore.datamodel.ifs2.MCRContent;
 import org.mycore.datamodel.ifs2.MCRFile;
 import org.mycore.datamodel.ifs2.MCRFileCollection;
 import org.mycore.datamodel.ifs2.MCRFileStore;
-import org.mycore.datamodel.ifs2.MCRNode;
-import org.mycore.datamodel.ifs2.MCRStore;
 import org.mycore.datamodel.ifs2.MCRStoreManager;
+
+import fsu.jportal.backend.impl.JournalListBackendImpl;
+import fsu.jportal.jaxb.JournalList;
 
 public class JournalListBackendImplTest {
     @Test
     public void getList() throws Exception {
-        MCRStoreManager.createStore("fake", FakeStore.class);
-    }
-    
-    class FakeStore extends MCRFileStore{
+        System.setProperty("MCR.IFS2.Store.fake.BaseDir", "fake");
+        System.setProperty("MCR.IFS2.Store.fake.SlotLayout", "4-2-2");
+        MCRFileStore fakeStore = MCRStoreManager.createStore("fake", MCRFileStore.class);
+        MCRFileCollection fileCollection = fakeStore.create(1);
+        MCRFile calenderListFile = fileCollection.createFile("calenderList");
         
+        
+//        calenderListFile.setContent(MCRContent.readFrom(calenderListStream));
+        JournalListBackendImpl journalListBackendImpl = new JournalListBackendImpl(fakeStore);
+        String listType = "calendar";
+        JournalList journalList = journalListBackendImpl.getList(listType);
+        
+        assertNotNull("Journal list " + listType + " should not be null",journalList);
     }
 }
