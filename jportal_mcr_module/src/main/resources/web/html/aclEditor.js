2e96c3b60780f3faf1682db2c0ac98f127a45127
Huu Chi Vu <chiliego.github@gmail.com>
2011-09-09 13:43:43 +0000

    ACL Editor GUI with javascript change Obj structure again

--- jportal_mcr_module/src/main/resources/web/html/aclEditor.js
+++ jportal_mcr_module/src/main/resources/web/html/aclEditor.js
@@ -1,4 +1,133 @@
 (function($) {
+    var AbstractGUI = function(){
+        var listeners = [];
+
+        return {
+            addListener : function(list) {
+                listeners.push(list);
+            },
+
+            notify : function(msg) {
+                for ( var i = 0; i < listeners.length; i++) {
+                    listeners[i].loadData(msg);
+                }
+            },
+
+            handleEvent : function(event) {
+                for ( var i = 0; i < controllReg.length; i++) {
+                    controllReg[i].handleEvent(event);
+                }
+            }
+        }
+        
+    };
+
+    var AccessList = function(spec){
+        var accesList = $('<ul/>').appendTo(spec.parentTag);
+        
+        accesList.toggleView = function(){
+            if(accesList.css('display') == 'none'){
+                accesList.show();
+            }else{
+                accesList.hide();
+            }
+        };
+        
+        return accesList;
+    };
+    
+    var RuleInfo = function(spec){
+        var parentTag =  spec.ParentTag;
+        
+        
+    };
+    
+    var ACLEditorGUI = function(spec){
+        var that = AbstractGUI();
+        var objIdList = $('<ul/>').appendTo(spec.parentTag);
+        
+        
+        that.fillObjIdList = function(data){
+            var li = $('<li/>').appendTo(objIdList);
+            var h3 = $('<h3/>').append(data.id).appendTo(li);
+            var accessList = null;
+            
+            h3.click(function(){
+                if(accessList == null){
+                    accessList = AccessList({parentTag: li});
+                    that.notify({
+                        link : data.link,
+                        callback : 'fillAccessIdList',
+                        parentTag : accessList
+                    });
+                }else {
+                    accessList.toggleView();
+                }
+            })
+        }
+        
+        that.fillAccessIdList = function(data){
+            var access = $('<li/>').appendTo(data.parentTag);
+            var accessView = $('<tr/>').appendTo($('<table/>').appendTo(access));
+            var accessId = $('<td/>').append(data.id).appendTo(accessView);
+            
+            that.notify({
+                link : data.link,
+                callback : 'fillRuleInfo',
+                parentTag : accessView
+            });
+        }
+        
+        that.fillRuleInfo = function(data){
+            var ruleInfo = RuleInfo(data);
+        }
+        
+        that.init = function(){
+            that.notify({
+                link : spec.link,
+                callback : 'fillObjIdList'
+            });
+        }
+        
+        return that;
+    };
+    
+    var GUIController = function(model){
+        var views = [];
+        return {
+            register : function(view) {
+                var viewListener = {
+                    loadData : function(msg) {
+                        var httpGET = model.getDataFromUrl(msg.link);
+                        var data = httpGET[msg.link];
+
+                        for ( var i = 0; i < data.length; i++) {
+                            var callbackData = data[i];
+                            callbackData.parentTag = msg.parentTag;
+                            view[msg.callback](callbackData);
+                        }
+                    }
+                };
+
+                view.addListener(viewListener);
+                views.push(view);
+
+                if (typeof view.init == 'function') {
+                    view.init();
+                }
+            },
+            
+            handleEvent : function(eventName){
+                for ( var i = 0; i < views.length; i++) {
+                    if(typeof views[i][eventName] == 'function'){
+                        views[i][eventName].apply(views[i]);
+                    }
+                }
+            }
+        }
+    };
+    
+    // ###############################################################
     function AbstractView(/* {listeners : array} */spec) {
         var listeners = (spec && spec.listeners) || [];
         var controllReg = (spec && spec.controllReg) || [];
@@ -26,6 +155,12 @@
                 for ( var i = 0; i < controllReg.length; i++) {
                     controllReg[i].register(view);
                 }
+            },
+            
+            handleEvent : function(event) {
+                for ( var i = 0; i < controllReg.length; i++) {
+                    controllReg[i].handleEvent(event);
+                }
             }
         }
     }
@@ -34,9 +169,21 @@
         var that = AbstractView();
         var objIDs = ObjIDList({parentTag : spec.parentTag});
         
+        spec.parentTag.selectable({
+            filter : 'li.selFilter'
+        });
+        
         that.init = function(){
             that.register(objIDs);
             objIDs.notify(spec.link);
+        };
+        
+        that.eventSelectableOff = function(){
+            spec.parentTag.selectable('option', 'distance', 20);
+        };
+        
+        that.eventSelectableOn = function(){
+            spec.parentTag.selectable('option', 'distance', 0);
         };
         
         return that;
@@ -49,6 +196,11 @@
             var h3 = $('<h3/>').appendTo(item.parentTag);
             var accessIDs = null;
             
+            h3.mouseover(function(){
+                that.handleEvent('eventSelectableOff');
+            }).mouseleave(function(){
+                that.handleEvent('eventSelectableOn');
+            });
             h3.append(item.data.id);
             h3.click(function(){
                 if(accessIDs == null){
@@ -65,10 +217,14 @@
     }
     
     function AccessIDList(spec){
-        var that = UlListGUI({parentTag : spec.parentTag});
+        var that = UlListGUI({
+            parentTag : spec.parentTag,
+            liClass : 'selFilter'
+            });
         
         function Button(spec){
             var button = $('<span/>').addClass(spec.icon).click(function() {
+                console.log('button click ' + spec.icon)
             });
             var textTag = $('<span/>');
             button.css({float : spec.float});
@@ -118,8 +274,10 @@
             for ( var i = 0; i < objs.length; i++) {
                 var currentObj = objs[i];
                 currentObj.mouseover(function(){
+                    that.handleEvent('eventSelectableOff');
                     colorAndToggle({color : '#cccccc', visibility: 'visible'})
                 }).mouseleave(function(){
+                    that.handleEvent('eventSelectableOn');
                     colorAndToggle({color : '', visibility: 'hidden'})
                 })
             }
@@ -158,6 +316,11 @@
         
         that.display = function(data){
             var li = $('<li/>').appendTo(ul);
+            
+            if(spec.liClass !== undefined){
+                li.addClass(spec.liClass);
+            }
+            
             if(typeof that.addItem == 'function'){
                 that.addItem({
                     parentTag : li,
@@ -180,6 +343,7 @@
     }
 
     function ViewController(model) {
+        var views = [];
         return {
             register : function(view) {
                 var viewListener = {
@@ -195,9 +359,18 @@
 
                 view.addListener(viewListener);
                 view.addRegister(this);
+                views.push(view);
 
                 if (typeof view.init == 'function') {
                     view.init();
+                }
+            },
+            
+            handleEvent : function(eventName){
+                for ( var i = 0; i < views.length; i++) {
+                    if(typeof views[i][eventName] == 'function'){
+                        views[i][eventName].apply(views[i]);
+                    }
                 }
             }
         }
@@ -234,6 +407,14 @@
 
         _create : function() {
             var model = Model(this.options.httpGET);
+            var guiController = GUIController(model);
+            var mainGUI = ACLEditorGUI({
+                parentTag : $(this.element[0]),
+                link : this.options.baseURL
+            });
+
+            guiController.register(mainGUI);
+            /*
             var guiController = ViewController(model);
             var mainGUI = MainGUI({
                 parentTag : $(this.element[0]),
@@ -241,297 +422,7 @@
             });
 
             guiController.register(mainGUI);
-
-            /*
-             * this.view = new this._view($(this.element[0])); this.model = new
-             * this._model(this.options.httpGET);
-             * 
-             * this.view.addListener(this);
-             * this.view.create(this.options.baseURL);
-             */
-        },
-
-        _view : function(mainTag) {
-            var editorID = mainTag.attr('id');
-            var classListID = editorID + 'ClassList';
-            var permColID = editorID + 'permCol';
-            var dropDownID = editorID + 'ruleListDropdown';
-
-            var classList = $('<div/>').attr({
-                id : classListID
-            }).css({
-                float : 'left'
-            }).appendTo(mainTag);
-            classList.selectable({
-                filter : 'li',
-                stop : function() {
-                    permCol.empty();
-                    var selectedObj = [];
-                    $(".ui-selected", this).each(function() {
-                        var objID = $(this).parentsUntil('div > h3').children('h3').html();
-                        var accessID = $('.id', this).html();
-
-                        selectedObj.push({
-                            id : objID,
-                            ac : accessID
-                        });
-                    });
-                    // notifyLoadPerms(selectedObj);
-                }
-            });
-
-            var permCol = $('<div/>').attr({
-                id : permColID
-            }).css({
-                float : 'left'
-            }).appendTo(mainTag);
-
-            var ruleList = $('<div/>').attr({
-                id : dropDownID
-            }).css({
-                float : 'left',
-                display : 'none'
-            }).appendTo(mainTag);
-
-            var listeners = [];
-            var mainTable = $('<table/>').appendTo(mainTag);
-
-            function notifyLoadData(url, target, callback) {
-                $.each(listeners, function(i, listener) {
-                    listener.loadData(url, target, callback);
-                })
-            }
-
-            function notifyLoadAllObjID() {
-                $.each(listeners, function(i, listener) {
-                    listener.loadAllObjID();
-                })
-            }
-
-            function notifyLoadAllAccess(id, target) {
-                $.each(listeners, function(i, listener) {
-                    listener.loadAllAccess(id, target);
-                })
-            }
-
-            function notifyLoadPerms(selectedObj) {
-                $.each(listeners, function(i, listener) {
-                    listener.loadPerms(selectedObj);
-                })
-            }
-
-            function notifyLoadPerm(id, target) {
-                $.each(listeners, function(i, listener) {
-                    listener.loadPerm(id, target);
-                })
-            }
-
-            function notifyClickChangeRule(target) {
-                $.each(listeners, function(i, listener) {
-                    listener.clickChangeRule(target);
-                })
-            }
-
-            function turnOffSelectable(/* List */objects, options) {
-                var _options = {
-                    mouseover : function() {
-                    },
-                    mouseleave : function() {
-                    }
-                };
-
-                if (options !== undefined) {
-                    _options = options;
-                }
-
-                $.each(objects, function(i, obj) {
-                    obj.mouseover(function() {
-                        classList.selectable('option', 'distance', 20);
-                        if (typeof _options.mouseover == 'function') {
-                            _options.mouseover();
-                        }
-                    }).mouseleave(function() {
-                        classList.selectable('option', 'distance', 0);
-                        if (typeof _options.mouseleave == 'function') {
-                            _options.mouseleave();
-                        }
-                    });
-                })
-            }
-
-            function multiCSS(objects, css) {
-                $.each(objects, function(i, obj) {
-                    obj.css(css)
-                })
-            }
-
-            this.create = function() {
-                notifyLoadAllObjID();
-            };
-
-            this.addListener = function(list) {
-                listeners.push(list);
-            };
-
-            this.displayObjId = function(obj, url) {
-                var id = obj.id;
-                var link = obj.link;
-                var classListDiv = $('<div/>').appendTo(classList);
-                var ul = $('<ul/>');
-                var h3 = $('<h3/>').html(id).appendTo(classListDiv).after(ul)
-
-                h3.click(function() {
-                    if (ul.children().size() < 1) {
-                        ul.show();
-                        notifyLoadAllAccess(link, ul);
-                    } else {
-                        if (ul.css('display') == 'none') {
-                            ul.show();
-                        } else {
-                            ul.hide();
-                        }
-                    }
-                });
-                turnOffSelectable([ h3 ]);
-            };
-
-            this.displayAccess = function(obj, target) {
-                var objId = obj.id;
-                var acId = obj.id;
-                var link = obj.link;
-
-                var table = $('<table/>').css({
-                    border : 'solid black 1px'
-                });
-
-                var id = $('<td class="id"/>').html(acId);
-
-                var permEdit = $('<span/>').addClass("ui-icon ui-icon-gear").click(function() {
-                    console.log('Click Edit');
-                });
-                var permChange = $('<span/>').addClass("ui-icon ui-icon-triangle-1-s").click(function() {
-                    console.log('Click Change');
-                    notifyChangePerm(this);
-                });
-
-                var permText = $('<span />');
-                var permDescrText = $('<span />');
-                var permCreatorText = $('<span />');
-
-                multiCSS([ permText, permDescrText, permCreatorText, permEdit, permChange ], {
-                    float : 'left'
-                });
-                multiCSS([ permEdit, permChange ], {
-                    visibility : 'hidden'
-                });
-
-                var perm = $('<td/>').append(permChange).append(permText);
-                var descr = $('<td/>').append(permDescrText);
-                var creator = $('<td/>').append(permCreatorText).append(permEdit);
-
-                turnOffSelectable([ perm, descr, creator ], {
-                    mouseover : function() {
-                        multiCSS([ permEdit, permChange ], {
-                            visibility : 'visible'
-                        });
-                        multiCSS([ perm, descr, creator ], {
-                            backgroundColor : '#cccccc'
-                        });
-                    },
-                    mouseleave : function() {
-                        multiCSS([ permEdit, permChange ], {
-                            visibility : 'hidden'
-                        });
-                        multiCSS([ perm, descr, creator ], {
-                            backgroundColor : ''
-                        });
-                    }
-                });
-
-                $('<tr/>').append(id).append(perm).append(descr).append(creator).appendTo(table);
-                $('<li/>').append(table).appendTo(target);
-                notifyLoadPerm(link, {
-                    perm : permText,
-                    descr : permDescrText,
-                    creator : permCreatorText
-                });
-            };
-
-            this.displayPerm = function(permObj, target) {
-                var notSet = '-/-'
-                var permId = permObj.rid !== undefined ? permObj.rid : notSet;
-                var permDescr = permObj.description !== undefined ? permObj.description : notSet;
-                var permCreator = permObj.creator !== undefined ? permObj.creator : notSet;
-
-                target.perm.html(permId);
-                target.descr.html(permDescr);
-                target.creator.html(permCreator);
-            }
-        },
-
-        _model : function(httpGET) {
-            var dataPool = {};
-
-            this.getDataFromUrl = function(url) {
-                var data = {};
-                var dataObj = $(dataPool).prop(url);
-
-                if (dataObj === undefined) {
-                    httpGET(url, {}, function(target, _data) {
-                        data = _data;
-                        $.extend(dataPool, data);
-                    });
-                } else {
-                    $(data).prop(url, dataObj);
-                }
-
-                return data;
-            };
-        },
-
-        loadAllObjID : function() {
-            var objIDs = this.model.getDataFromUrl(this.options.baseURL);
-            var view = this.view;
-
-            $.each(objIDs, function(url, data) {
-                $.each(data, function(index, obj) {
-                    view.displayObjId(obj, url);
-                });
-            })
-        },
-
-        loadAllAccess : function(url, target) {
-            var allAccess = this.model.getDataFromUrl(url);
-            var view = this.view;
-
-            $.each(allAccess, function(url, data) {
-                $.each(data, function(index, obj) {
-                    view.displayAccess(obj, target);
-                })
-            });
-        },
-
-        loadPerm : function(url, target) {
-            var permObj = this.model.getDataFromUrl(url);
-            var view = this.view;
-
-            $.each(permObj, function(url, data) {
-                $.each(data, function(index, obj) {
-                    view.displayPerm(obj, target);
-                })
-            });
-        },
-
-        clickChangeRule : function(target) {
-            var rules = this.model.getDataFromUrl(this.options.ruleURL);
-            var permObj = this.model.getDataFromUrl(url);
-            var view = this.view;
-
-            $.each(permObj, function(url, data) {
-                $.each(data, function(index, obj) {
-                    view.displayPerm(obj, target);
-                })
-            });
+            */
         }
     });
 })(jQuery);
