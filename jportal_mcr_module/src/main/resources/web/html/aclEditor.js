128c8b300c216d9cb76c00671ac331035f3f32e3
Huu Chi Vu <chiliego.github@gmail.com>
2011-09-20 14:53:12 +0000

    simplify ACL editor

--- jportal_mcr_module/src/main/resources/web/html/aclEditor.js
+++ jportal_mcr_module/src/main/resources/web/html/aclEditor.js
@@ -19,241 +19,90 @@
     
     var MainView = function(conf){
         var mainView = AbstractView();
-        var ul = $('<ul/>').appendTo(conf.parentTag);
+        var table = $('<table/>').appendTo(conf.parentTag);
+        var head = $('<tr/>').append('<th>objId</th><th>acpool</th><th>rule</th>').appendTo(table);
+        function getRuleDescr(rid){
+            var rule = '--/--';
+            if(rid !== undefined){
+                rule = rid;
+                mainView.notify({
+                    event : 'loadRule',
+                    args : {
+                        rid : rid,
+                        callback : function(data){
+                            if(data.descr !== undefined){
+                                rule = data.descr;
+                            }
+                        }
+                    }
+                });
+            }
+            
+            return rule;
+        }
         
         mainView.create = function(){
             mainView.notify({
-                event : 'loadObjIds',
-                link : conf.link
+                event : 'loadAccess'
             });
         };
         
-        mainView.addObjId = function(data){
-            $('<li/>').append(data.id).appendTo(ul).click(function(){
-                mainView.notify({
-                    event : 'loadAccess',
-                    link : data.link
-                });
-            });
+        mainView.addAccess = function(url, data){
+            var tr = $('<tr/>').appendTo(table);
+            $('<td/>').append(data.objid).appendTo(tr);
+            $('<td/>').append(data.acpool).appendTo(tr);
+            $('<td/>').append(getRuleDescr(data.rid)).appendTo(tr);
         };
         return mainView;
     }
     
     var Controller = function(view, model){
         var MainViewHandler = function(){
-            
+            return {
+                loadAccess : function(){
+                    var access = model.getAccess();
+                    $.each(access, function(url, data){
+                        view.addAccess(url, data);
+                    });
+                },
+                loadRule : function(args){
+                    var rule = model.getRule(args.rid);
+                    args.callback(rule);
+                }
+            }
         }
+        view.addHandlers(MainViewHandler());
+        view.create()
     };
     
-    var Editor = function(conf) {
-        var model = conf.model;
-
-        var mainList = $('<ul/>').appendTo(conf.parentTag).selectable({
-            filter : 'li.ui-selectee'
-        }).bind('selOff', function(e) {
-            $(this).selectable({
-                distance : '20'
-            });
-        }).bind('selOn', function(e) {
-            $(this).selectable({
-                distance : '0'
-            });
+    var Model = function(conf) {
+        var access = {};
+        var rules = {};
+        
+        conf.httpGET(conf.accessURL, function(data){
+            access = data;
         });
-
-        var ruleSelBox = function() {
-            var selBoxFrame = $('<div/>').appendTo(conf.parentTag).hide();
-            var selBox = $('<ul/>').appendTo(selBoxFrame);
-            var connectedObjs = $({});
-            var connectedFn = function(){};
-            var connectedSelected = function(){};
-            
-            $.each(model.getDataFromUrl(conf.rulesUrl), function(i, data) {
-                var li = $('<li/>').append(data.id).appendTo(selBox);
-                var oldColor = '';
-                li.hover(function(){
-                    oldColor = li.css('backgroundColor');
-                    li.css({
-                        backgroundColor : 'yellow',
-                        cursor: 'pointer'
-                    });
-                }, function(){
-                    li.css({
-                        backgroundColor : oldColor,
-                        cursor: 'auto'
-                    });
-                }).click(function(){
-                    var ruleData = model.getDataFromUrl(data.link)[0];
-                    connectedSelected(ruleData);
-                });
-            });
-            
-            selBoxFrame.css({
-                backgroundColor : '#cccccc',
-                width : '200px',
-                maxHeight : '322px',
-                overflow : 'auto'
-            })
-            
-            
-            selBoxFrame.mouseleave(function() {
-                connectedObjs.trigger('mouseleave');
-                selBoxFrame.hide();
-                connectedObjs = $({});
-                connectedFn = function(){};
-            }).mouseover(function(){
-                connectedFn();
-            });
-            
-            return {
-                show : function(css){
-                    selBoxFrame.trigger('mouseover');
-                    selBoxFrame.trigger('mouseleave');
-                    selBoxFrame.show();
-                    selBoxFrame.css(css);
-                    return this;
-                },
-                hide : function(){
-                    selBoxFrame.trigger('mouseover');
-                    selBoxFrame.trigger('mouseleave');
-                },
-                connect : function(to){
-                    connectedObjs = to.objs;
-                    connectedFn = to.fn;
-                    connectedSelected = to.selected;
-                    return this;
-                }
-            };
-        }();
-
-        function loadAccess(url) {
-            var ul = $('<ul/>');
-            $.each(model.getDataFromUrl(url), function(i, data) {
-                var li = $('<li class="ui-acleditor-access ui-selectee"/>').appendTo(ul);
-                var tr = $('<tr/>')
-                var ruleData = model.getDataFromUrl(data.link)[0];
-                var ridTxt = typeof ruleData.rid == 'string' ? ruleData.rid : '--/--';
-                var descrTxt = typeof ruleData.description == 'string' ? ruleData.description : '--/--';
-                var creatorTxt = typeof ruleData.creator == 'string' ? ruleData.creator : '--/--';
-
-                tr.append('<td class="acId">' + data.id + '</td>');
-                tr.append('<td class="rule button change"><span class="ui-icon ui-icon-triangle-1-s" style="visibility:hidden"/></td>');
-                tr.append('<td class="rule id">' + ridTxt + '</td>');
-                tr.append('<td class="rule descr">' + descrTxt + '</td>');
-                tr.append('<td class="rule creator">' + creatorTxt + '</td>');
-                tr.append('<td class="rule button edit"><span class="ui-icon ui-icon-gear ui-rule-button"/></td>');
-                tr.appendTo($('<table/>').css({
-                    borderCollapse : 'collapse'
-                }).appendTo(li));
-
-                tr.delegate('.rule', 'mouseover', function() {
-                    $('.rule', tr).addClass('ui-rule-selected');
-                    $('.rule.button>span', tr).css({
-                        visibility : 'visible'
-                    });
-                });
-
-                var ruleMarkingOnMouseLeave = function() {
-                    $('.rule', tr).removeClass('ui-rule-selected');
-                    $('.rule.button>span', tr).css({
-                        visibility : 'hidden'
-                    });
-                }
-                tr.delegate('.rule', 'mouseleave', function() {
-                    ruleMarkingOnMouseLeave();
-                });
-
-                tr.delegate('.rule.button', 'mouseover', function() {
-                    mainList.trigger('selOff');
-                });
-
-                tr.delegate('.rule.button', 'mouseleave', function() {
-                    mainList.trigger('selOn');
-                });
-
-                var hidden = true;
-                var showSelBox = function(position){
-                    hidden = false;
-                    var safeRuleMarkingOnMouseLeave = ruleMarkingOnMouseLeave;
-                    ruleMarkingOnMouseLeave = function() {
-                    };
-                    
-                    ruleSelBox.show({
-                        position : 'absolute',
-                        top : position.top + $('table', li).outerHeight(),
-                        left : position.left,
-                    }).connect({
-                        objs : $('.rule', tr),
-                        fn : function(){
-                            ruleMarkingOnMouseLeave = safeRuleMarkingOnMouseLeave
-                            hidden = true;
-                        },
-                        selected: function(ruleData){
-                            var ridTxt = typeof ruleData.rid == 'string' ? ruleData.rid : '--/--';
-                            var descrTxt = typeof ruleData.description == 'string' ? ruleData.description : '--/--';
-                            var creatorTxt = typeof ruleData.creator == 'string' ? ruleData.creator : '--/--';
-                            $('.rule.id', tr).html(ridTxt);
-                            $('.rule.descr', tr).html(descrTxt);
-                            $('.rule.creator', tr).html(creatorTxt);
-                        } 
-                    });
-                };
-                
-                var hideSelBox = function(){
-                    hidden = true;
-                    ruleSelBox.hide();
-                }
-                $('.rule.change.button', tr).click(function() {
-                    var position = $(this).position();
-                    
-                    if(hidden){
-                        showSelBox(position)
-                    }else{
-                        hideSelBox();
-                    }
-                });
-
-                $('.rule.edit.button', tr).click(function() {
-                    console.log('edit rule');
-                })
-            });
-
-            return ul;
-        }
-
-        $.each(model.getDataFromUrl(conf.startUrl), function(i, data) {
-            var li = $('<li class="ui-acleditor-objId"/>').appendTo(mainList);
-
-            $('<h3 class="ui-acleditor-objId-txt"/>').append(data.id).appendTo(li).click(function(event) {
-                var access = $(this).data('access')
-                if (access === undefined) {
-                    $(this).data('access', loadAccess(data.link).appendTo(li));
-                } else {
-                    access.fadeToggle('fast');
-                }
-            }).mouseover(function() {
-                mainList.trigger('selOff');
-            }).mouseleave(function() {
-                mainList.trigger('selOn');
-            });
+        
+        conf.httpGET(conf.rulesURL, function(data){
+            rules = data;
         });
-    }
-
-    var Model = function(httpGET) {
-        var dataPool = {};
-
         return {
-            getDataFromUrl : function(url) {
-                var dataObj = dataPool[url];
-
-                if (dataObj === undefined) {
-                    httpGET(url, {}, function(target, data) {
-                        dataObj = data[url];
-                        dataPool[url] = dataObj;
-                    });
+            getAccess : function(url) {
+                if(url === undefined){
+                    return access;
+                }else{
+                    return access[url];
                 }
-
-                return dataObj;
-            }
+            },
+            
+            getRule : function(rid) {
+                if(rid === undefined){
+                    return rules;
+                }else{
+                    var url = conf.rulesURL + '/' + rid;
+                    return rules[url];
+                }
+            },
         }
     }
 
@@ -265,20 +114,11 @@
         },
 
         _create : function() {
-            var model = Model(this.options.httpGET);
-            /*
-             * console.log('rules url create ' + this.options.rulesURL) var
-             * mainWindow = ACLEditorMainWindow({ startUrl :
-             * this.options.baseURL, rulesUrl : this.options.rulesURL, parentTag :
-             * this.element[0] }); var mainWindowController =
-             * ACLEditorController(model, mainWindow);
-             */
-            Editor({
-                model : model,
-                startUrl : this.options.baseURL,
-                rulesUrl : this.options.rulesURL,
+            var model = Model(this.options);
+            var view = MainView({
                 parentTag : this.element[0]
-            })
+            });
+            Controller(view,model);
         }
     });
 })(jQuery);
--- jportal_mcr_module/src/main/resources/web/html/aclEditorTest.js
+++ jportal_mcr_module/src/main/resources/web/html/aclEditorTest.js
@@ -4,80 +4,32 @@
 $(document).ready(function() {
         console.log('Ready!');
         var editorConfig = {
-                baseURL : 'http://localhost:8291/rsc/acl/rsc',
-                rulesURL : 'http://localhost:8291/rsc/acl/rules',
-                rulesListURL: 'http://localhost:8291/rsc/acl/rules',
-                httpGET: function(url, target, callBack){
+                accessURL : 'http://localhost:8291/rsc/acl/mcraccess',
+                rulesURL : 'http://localhost:8291/rsc/acl/mcraccessrule',
+                httpGET: function(url, callBack){
                     var data = {
-                            'http://localhost:8291/rsc/acl/rsc' : [
-                                {objid: 'fsu.jportal.resources.FOOResource', link: url + '/FOOResource'},
-                                {objid: 'fsu.jportal.resources.ACLResource', link: url + '/ACLResource'},
-                                {objid: 'fsu.jportal.resources.JournalClassificationResource', link : url + '/JournalClassificationResource'}
-                                ],
-                            'http://localhost:8291/rsc/acl/rsc/FOOResource' : [
-                                {id : '/acl/rsc_GET', link : 'http://System000001'}, 
-                                {id : '/acl/rsc_POST', link : url + '/acl/rsc_POST'}
-                                ],
-                            'http://localhost:8291/rsc/acl/rsc/ACLResource' : [
-                                {id: '/classifications/jp/{id}/save_POST', link : url + '/classifications/jp/{id}/save_POST'}
-                                ],
-                            'http://localhost:8291/rsc/acl/rsc/JournalClassificationResource' : [
-                                {id: '/classifications/foo/save_POST',link : url + '/classifications/foo/save_POST'}
-                                ],
-                            'http://System000001' : [{
-                                rid: 'System000001',
-                                creator: 'admin',
-                                creationdate: 'heute',
-                                rule: 'true',
-                                description: 'fakerule',
-                                }],
-                            'http://System000002' : [{
-                                rid: 'System000002',
-                                creator: 'chi',
-                                creationdate: 'morgen',
-                                rule: 'false',
-                                description: 'dummy',
-                                }],
-                            'http://localhost:8291/rsc/acl/rules' : [
-                              {id: 'System000001', link : 'http://System000001'},
-                              {id: 'System000002', link : 'http://System000002'},
-                              {id: 'System000003', link : 'http://System000003'},
-                              {id: 'System000004', link : 'http://System000004'},
-                              ] 
+                            'http://localhost:8291/rsc/acl/mcraccess' : {
+                                'url1' : {objid: 'fsu.jportal.resources.FOOResource', acpool : '/acl/rsc_GET', rid : 'System000001'},
+                                'url2' : {objid: 'fsu.jportal.resources.FOOResource', acpool : '/acl/rsc_POST'},
+                                'url3' : {objid: 'fsu.jportal.resources.ACLResource', acpool : '/classifications/jp/{id}/save_POST', rid : 'System000003'},
+                                'url4' : {objid: 'fsu.jportal.resources.JournalClassificationResource', acpool : '/classifications/foo/save_POST'}
+                            },
+                            'http://localhost:8291/rsc/acl/mcraccessrule' : {
+                                'http://localhost:8291/rsc/acl/mcraccessrule/System000001' : {rid: 'System000001', rule : 'true', descr : 'fakerule'},
+                                'http://localhost:8291/rsc/acl/mcraccessrule/System000002' : {rid: 'System000002', rule : 'false', descr : 'dummy'},
+                                'http://localhost:8291/rsc/acl/mcraccessrule/System000003' : {rid: 'System000003', rule : 'false or true'},
+                            }
                     };
                     
                     var retrievedData = {};
                     if(data[url] !== undefined){
-                        retrievedData[url] = data[url];
+                        retrievedData = data[url];
                     }else{
-                        retrievedData[url] = [{}];
                         console.log("default url: " + url);
                     }
-                    callBack(target, retrievedData);
+                    callBack(retrievedData);
                 }
         };
         
-        var editorConfig1 = {
-                baseURL : 'http://localhost:8291/rsc/acl/rsc'
-        };
-        
-        var obj = function(){
-            return {
-                print : function(){
-                    console.log('hello');
-                }
-            }
-        }();
-         var extObj = function(){
-             var parent = obj
-             var superPrint = obj.print;
-             parent.print = function(){
-                 superPrint();
-                 
-             }
-             obj['print']()
-             return parent;
-         }
         $('#aclEditor').aclEditor(editorConfig);
-        
  })
