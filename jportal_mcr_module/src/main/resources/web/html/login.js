fef843314de18f13bebdfddcf0e3f4850fdfa42b
Huu Chi Vu <chiliego.github@gmail.com>
2011-12-10 13:38:10 +0000

    Add experiments module

---
+++ jportal_mcr_module/src/main/resources/web/html/login.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+$(document).ready(function(){
+    $("#dialog-form").dialog({
+        resizable: false,
+        height: 204,
+        width: 222,
+        modal: true,
+        buttons : {
+            "Anmelden" : function(){
+                var userID = $('#login').val();
+                var password = $('#password').val();
+                console.log('user: ' + userID + ' pass: ' + password);
+                $.ajax({
+                    type :  'POST',
+                    url : 'http://localhost:8291/rsc/login',
+                    data : '{userID: ' + userID+', password:'+password+'}',
+                    contentType: "application/json; charset=utf-8",
+                    success : function(data){
+                        console.log('data: ' + data);
+                    }
+                })
+            },
+            "Abbrechen" : function(){
+                
+            }
+        }
+    })
+})
