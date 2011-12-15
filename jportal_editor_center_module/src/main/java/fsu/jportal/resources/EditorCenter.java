6e1d27939fc14c70fc628a5c42f3cd2592b12a1c
Huu Chi Vu <chiliego.github@gmail.com>
2011-12-15 10:38:16 +0000

    add error diag and progress diag

--- jportal_editor_center_module/src/main/java/fsu/jportal/resources/EditorCenter.java
+++ jportal_editor_center_module/src/main/java/fsu/jportal/resources/EditorCenter.java
@@ -6,11 +6,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import org.mycore.frontend.cli.MCRJPortalRedundancyCommands;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import fsu.jportal.gson.GsonManager;
+
 @Path("editorCenter")
 public class EditorCenter {
     @GET
     @Path("{filename:.*}")
     public InputStream getResources(@PathParam("filename") String filename){
         return this.getClass().getResourceAsStream("/editorCenter/" + filename);
+    }
+    
+    @GET
+    @Path("numDoubletsOf/{type}")
+    public String getNumOfDoublets(@PathParam("type") String type){
+        int numOfDoublets = MCRJPortalRedundancyCommands.getDoubletObjsOfType(type).getNumHits();
+        Gson gson = GsonManager.instance().createGson();
+        JsonObject numDubletJson = new JsonObject();
+        numDubletJson.addProperty("num", numOfDoublets);
+        return gson.toJson(numDubletJson);
     }
 }
--- jportal_editor_center_module/src/main/resources/editorCenter/editorCenter.js
+++ jportal_editor_center_module/src/main/resources/editorCenter/editorCenter.js
@@ -4,17 +4,46 @@
 
 $(document).ready(function(){
 //    $('#toolbar > div').resizable();
+    function nonResizeableDiag(id){
+        return $(id).dialog({ 
+            modal: true,
+            autoOpen: false,
+            resizable: false
+        });
+    }
+    
+    function removeDuplicateForType(type){
+        $('#progress').dialog('open');
+        $.get('/servlets/MCRWebCLIServlet?run=jp clean up ' + type, function(data){
+            console.log('remove person');
+            $('#progress').dialog('close');
+        });
+    }
+    
     $('#tabs').tabs();
     $('#tabs, .ui-tabs-nav').removeClass('ui-corner-all');
     $('#tabs').addClass('ui-corner-bottom');
-    $('#removePerson').button().click(function(){
-        $.get('/servlets/MCRWebCLIServlet?request=getKnownCommands', function(data){
-            console.log('Data: ' + data);
-        }).error(function(err){
-            console.log("Error: " + err);
-        });
-        $.get('/servlets/MCRWebCLIServlet?run=jp clean up person', function(data){
-            console.log('remove person');
-        });
+    $('#removePerson').button({'disabled': true});
+    
+    nonResizeableDiag('#errMsg');
+    nonResizeableDiag('#progress').parent().find('a.ui-dialog-titlebar-close').hide();
+    
+    $.getJSON('/rsc/editorCenter/numDoubletsOf/person', function(data){
+        if(parseInt(data.num) > 0){
+            $('#removePerson').button('enable');
+            
+            $('#numDuplicatesPers').html(data.num);
+            
+            $('#removePerson').click(function(){
+                $.get('/servlets/MCRWebCLIServlet?request=getKnownCommands', function(data){
+                    console.log('Data: ' + data);
+                    removeDuplicateForType('person');
+                }).error(function(err){
+                    if(err.status == 403){
+                        $('#errMsg').dialog('open');
+                    }
+                });
+            })
+        }
     })
 })
