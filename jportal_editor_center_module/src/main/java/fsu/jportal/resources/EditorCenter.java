fa5cd5e2f5d2b0b21278d74729ff4591cda4fd58
Huu Chi Vu <chiliego.github@gmail.com>
2011-12-20 12:43:01 +0000

    first beta release for editor center

--- jportal_editor_center_module/src/main/java/fsu/jportal/resources/EditorCenter.java
+++ jportal_editor_center_module/src/main/java/fsu/jportal/resources/EditorCenter.java
@@ -2,11 +2,15 @@ package fsu.jportal.resources;
 
 import java.io.InputStream;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 import org.mycore.frontend.cli.MCRJPortalRedundancyCommands;
+import org.mycore.frontend.cli.MCRKnownCommands;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -29,5 +33,21 @@ public class EditorCenter {
         JsonObject numDubletJson = new JsonObject();
         numDubletJson.addProperty("num", numOfDoublets);
         return gson.toJson(numDubletJson);
+    }
+    
+    @POST
+    @Path("rmDuplicates/{type}")
+    @RolesAllowed("")
+    public Response removeDuplicatesFor(@PathParam("type") String type){
+        MCRKnownCommands mcrKnownCommands = new MCRKnownCommands();
+        try {
+            mcrKnownCommands.invokeCommand("jp clean up " + type);
+            return Response.ok().build();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+//        MCRJPortalRedundancyCommands.cleanUp(type);
     }
 }
--- jportal_editor_center_module/src/main/resources/editorCenter/editorCenter.js
+++ jportal_editor_center_module/src/main/resources/editorCenter/editorCenter.js
@@ -12,38 +12,79 @@ $(document).ready(function(){
         });
     }
     
-    function removeDuplicateForType(type){
-        $('#progress').dialog('open');
-        $.get('/servlets/MCRWebCLIServlet?run=jp clean up ' + type, function(data){
-            console.log('remove person');
-            $('#progress').dialog('close');
-        });
+    function initView(){
+        var tabs = $('#tabs').tabs();
+        
+        $('#tabs, .ui-tabs-nav').removeClass('ui-corner-all');
+        tabs.addClass('ui-corner-bottom');
+        tabs.find('.removeButton').button({'disabled': true});
+        nonResizeableDiag('#errMsg');
+        nonResizeableDiag('#progress').parent().find('a.ui-dialog-titlebar-close').hide();
     }
     
-    $('#tabs').tabs();
-    $('#tabs, .ui-tabs-nav').removeClass('ui-corner-all');
-    $('#tabs').addClass('ui-corner-bottom');
-    $('#removePerson').button({'disabled': true});
-    
-    nonResizeableDiag('#errMsg');
-    nonResizeableDiag('#progress').parent().find('a.ui-dialog-titlebar-close').hide();
-    
-    $.getJSON('/rsc/editorCenter/numDoubletsOf/person', function(data){
-        if(parseInt(data.num) > 0){
-            $('#removePerson').button('enable');
-            
-            $('#numDuplicatesPers').html(data.num);
-            
-            $('#removePerson').click(function(){
-                $.get('/servlets/MCRWebCLIServlet?request=getKnownCommands', function(data){
-                    console.log('Data: ' + data);
-                    removeDuplicateForType('person');
-                }).error(function(err){
-                    if(err.status == 403){
-                        $('#errMsg').dialog('open');
-                    }
-                });
+    var TabController = function(type){
+        var id = '#' + type;
+        var counter = $(id).find('.counter');
+        var removeButton = $(id).find('.removeButton');
+        var errMsgDiag = $('#errMsg');
+        var progressDiag = $('#progress');
+        
+        function updateCounter(){
+            $.getJSON('/rsc/editorCenter/numDoubletsOf/' + type, function(data){
+                counter.html(data.num);
+                if(parseInt(data.num) > 0){
+                    removeButton.button('enable');
+                } else{
+                    removeButton.button('disable');
+                }
             })
         }
-    })
+        
+        var proc = 0;
+        function processing(){
+            if(proc == 0){
+                $.ajax({ 
+                    type :  'GET',
+                    url: '/servlets/MCRWebCLIServlet?request=getStatus', 
+                    success: function(data){
+                        if(data.running == false){
+                            setTimeout(function(){
+                                $('#progress').dialog('close');
+                                updateCounter();
+                            }, 2000);
+                            proc = 100;
+                        }
+                    }, 
+                    complete: processing, 
+                    timeout: 1000
+                });
+            } else {
+                proc = 0;
+            }
+        }
+        
+        
+        function removeDuplicate(){
+            $.get('/servlets/MCRWebCLIServlet?run=jp clean up ' + type, function(data){
+                progressDiag.dialog('open');
+                processing();
+            });
+        }
+        
+        removeButton.click(function(){
+            $.get('/servlets/MCRWebCLIServlet?request=getKnownCommands', function(data){
+                removeDuplicate();
+            }).error(function(err){
+                if(err.status == 403){
+                    errMsgDiag.dialog('open');
+                }
+            });
+        })
+        
+        updateCounter();
+    }
+    
+    initView()
+    new TabController('person');
+    new TabController('jpinst');
 })
