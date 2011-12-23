67ff008eab545b39d9a6c8e7c2207ce7ac8b86b0
Huu Chi Vu <chiliego.github@gmail.com>
2011-12-23 14:23:10 +0000

    add animated progressbar

--- jportal_editor_center_module/src/main/resources/editorCenter/editorCenter.js
+++ jportal_editor_center_module/src/main/resources/editorCenter/editorCenter.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
         $('#tabs, .ui-tabs-nav').removeClass('ui-corner-all');
         tabs.addClass('ui-corner-bottom');
         tabs.find('.removeButton').button({'disabled': true});
+        $('#progressbar').progressbar({value: 0});
         nonResizeableDiag('#errMsg');
         nonResizeableDiag('#progress').parent().find('a.ui-dialog-titlebar-close').hide();
     }
@@ -28,10 +29,14 @@ $(document).ready(function(){
         var removeButton = $(id).find('.removeButton');
         var errMsgDiag = $('#errMsg');
         var progressDiag = $('#progress');
+        var progressBar = $('#progressbar');
+        var remainderDispl = $('#remainder');
+        var numOfDuplicates = 0;
         
         function updateCounter(){
             $.getJSON('/rsc/editorCenter/numDoubletsOf/' + type, function(data){
-                counter.html(data.num);
+                numOfDuplicates = data.num;
+                counter.html(numOfDuplicates);
                 if(parseInt(data.num) > 0){
                     removeButton.button('enable');
                 } else{
@@ -40,33 +45,62 @@ $(document).ready(function(){
             })
         }
         
+        function updateProgressBar(remainder){
+            remainderDispl.html(remainder);
+            if(remainder == 0){
+                progressBar.progressbar({value : 100});
+            } else {
+                var percent = (numOfDuplicates / remainder) * 100;
+                progressBar.progressbar({value : percent});
+            }
+        }
+        
         var proc = 0;
+        var remainder = 0;
         function processing(){
+            $.get('/servlets/MCRWebCLIServlet?request=getCommandQueue', function(data){
+                var tmpRemainder = data.commandQueue.length;
+                
+                if(tmpRemainder < remainder){
+                    remainder = tmpRemainder;
+                }
+            });
+            
             if(proc == 0){
                 $.ajax({ 
                     type :  'GET',
                     url: '/servlets/MCRWebCLIServlet?request=getStatus', 
                     success: function(data){
+                        updateProgressBar(remainder);
                         if(data.running == false){
                             setTimeout(function(){
-                                $('#progress').dialog('close');
                                 updateCounter();
+                                closeProgressDiag();
                             }, 2000);
                             proc = 100;
                         }
                     }, 
                     complete: processing, 
-                    timeout: 1000
+                    timeout: 2000
                 });
             } else {
                 proc = 0;
             }
         }
         
+        function openProgressDiag(){
+            remainderDispl.html(numOfDuplicates);
+            progressDiag.dialog('open');
+        }
+        
+        function closeProgressDiag(){
+            remainderDispl.html(0);
+            progressDiag.dialog('close');
+        }
         
         function removeDuplicate(){
             $.get('/servlets/MCRWebCLIServlet?run=jp clean up ' + type, function(data){
-                progressDiag.dialog('open');
+                openProgressDiag();
                 processing();
             });
         }
