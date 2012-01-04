abf268792b2fd38fe37e9bc487e75fbe4a30ef79
Huu Chi Vu <chiliego.github@gmail.com>
2012-01-04 14:59:19 +0000

    some more experiments

--- jportal_experiments/src/main/java/fsu/jportal/resources/JportalExperimentsResource.java
+++ jportal_experiments/src/main/java/fsu/jportal/resources/JportalExperimentsResource.java
@@ -2,16 +2,63 @@ package fsu.jportal.resources;
 
 
 import java.io.InputStream;
+import java.util.Date;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
-@Path("html")
+import org.jdom.Document;
+import org.jdom.Element;
+import org.mycore.common.MCRPersistenceException;
+import org.mycore.common.events.MCREvent;
+import org.mycore.common.events.MCREventManager;
+import org.mycore.datamodel.common.MCRActiveLinkException;
+import org.mycore.datamodel.common.MCRXMLMetadataManager;
+import org.mycore.datamodel.metadata.MCRMetadataManager;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.datamodel.metadata.MCRObjectID;
+
+import fsu.jportal.resources.filter.MyCoReSecurityFilterFactory.MCRDBAccess;
+
+@Path("exp")
 public class JportalExperimentsResource {
     @GET
     @Path("{filename:.*}")
     public InputStream getResources(@PathParam("filename") String filename){
         return this.getClass().getResourceAsStream("/html/" + filename);
+    }
+    
+    @POST
+    @MCRDBAccess
+    @Path("cp/{id}")
+    public Response copy(@PathParam("id") String id, @QueryParam("numCopy") int numCopy){
+        MCRObjectID mcrId = MCRObjectID.getInstance(id);
+        String base = mcrId.getBase();
+        Document origObjXML = MCRXMLMetadataManager.instance().retrieveXML(mcrId);
+        Element origObjXMLRootTag = origObjXML.getRootElement();
+        
+        for (int i = 0; i < numCopy; i++) {
+            MCRObjectID nextFreeId = MCRObjectID.getNextFreeId(base);
+            origObjXMLRootTag.setAttribute("ID", nextFreeId.toString());
+            origObjXMLRootTag.setAttribute("label", nextFreeId.toString());
+            
+            
+            try {
+                MCRMetadataManager.create(new MCRObject(origObjXML));
+//                MCRXMLMetadataManager.instance().create(nextFreeId, origObjXML, new Date());
+            } catch (MCRPersistenceException e) {
+                e.printStackTrace();
+                return Response.serverError().build();
+            } catch (MCRActiveLinkException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+        return Response.ok().build();
     }
 }
