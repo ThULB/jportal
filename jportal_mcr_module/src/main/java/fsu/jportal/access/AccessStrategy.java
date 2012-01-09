46d2da378226711fe115ac94e873dc1dea8c6800
Huu Chi Vu <chiliego.github@gmail.com>
2012-01-09 13:01:04 +0000

    increase version to 2.15

--- jportal_mcr_module/src/main/java/fsu/jportal/access/AccessStrategy.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/access/AccessStrategy.java
@@ -3,11 +3,6 @@ package fsu.jportal.access;
 import java.util.HashMap;
 
 import org.apache.log4j.Logger;
-import org.jdom.Attribute;
-import org.jdom.Document;
-import org.jdom.JDOMException;
-import org.jdom.Text;
-import org.jdom.xpath.XPath;
 import org.mycore.access.MCRAccessInterface;
 import org.mycore.access.MCRAccessManager;
 import org.mycore.access.strategies.MCRAccessCheckStrategy;
@@ -15,13 +10,14 @@ import org.mycore.access.strategies.MCRObjectIDStrategy;
 import org.mycore.access.strategies.MCRObjectTypeStrategy;
 import org.mycore.access.strategies.MCRParentRuleStrategy;
 import org.mycore.common.MCRConfiguration;
-import org.mycore.common.MCRConstants;
 import org.mycore.common.MCRException;
 import org.mycore.common.MCRSessionMgr;
 import org.mycore.datamodel.common.MCRXMLMetadataManager;
 import org.mycore.datamodel.metadata.MCRObjectID;
 
 public class AccessStrategy implements MCRAccessCheckStrategy {
+    private StrategyStep strategyChainStart;
+    
     private class InvalidIDException extends Exception {
 
     }
@@ -62,23 +58,12 @@ public class AccessStrategy implements MCRAccessCheckStrategy {
 
     public AccessStrategy(AccessStrategyConfig accessConfig) {
         setAccessConfig(accessConfig);
+        initStrategyChain();
     }
 
-    public boolean checkPermission(String id, String permission) {
-        MCRAccessInterface accessInterface = getAccessConfig().getAccessInterface();
-
-        if (id == null || "".equals(id) || permission == null || "".equals(permission)) {
-            return false;
-        }
-
-        if (isSuperUser()) {
-            return true;
-        }
-
-        return checkPermForObj(id, permission, getAccessConfig());
-    }
-    
-    protected boolean checkPermForObj(String id, String permission, AccessStrategyConfig accessStrategyConfig) {
+    protected void initStrategyChain() {
+        AccessStrategyConfig accessStrategyConfig = getAccessConfig();
+        
         StrategyStep objIdCheck = new ObjIdCheck(accessStrategyConfig);
         StrategyStep crudCheck = new CRUDCheck(accessStrategyConfig);
         StrategyStep parentCheck = new ParentCheck(accessStrategyConfig);
@@ -87,10 +72,21 @@ public class AccessStrategy implements MCRAccessCheckStrategy {
         parentCheck.addAlternative(defaultCheck);
         crudCheck.addAlternative(parentCheck);
         objIdCheck.addAlternative(crudCheck);
-        
-        return objIdCheck.checkPermission(id, permission);
+        setStrategyChainStart(objIdCheck);
     }
+    
+    public boolean checkPermission(String id, String permission) {
+        if (id == null || "".equals(id) || permission == null || "".equals(permission)) {
+            return false;
+        }
 
+        if (isSuperUser()) {
+            return true;
+        }
+
+        return getStrategyChainStart().checkPermission(id, permission);
+    }
+    
     protected MCRObjectID getObjId(String id) throws InvalidIDException {
         try {
             MCRObjectID objID = MCRObjectID.getInstance(id);
@@ -98,39 +94,6 @@ public class AccessStrategy implements MCRAccessCheckStrategy {
         } catch (MCRException e) {
             throw new InvalidIDException();
         }
-    }
-
-    private boolean isCRUD_Operation(String permission) {
-        return permission.startsWith("create_") || permission.startsWith("read_") || permission.startsWith("update_")
-                || permission.startsWith("delete_");
-    }
-
-    private MCRObjectID getParentID(MCRObjectID objID) {
-        //LOGGER.info("getParentId: " + objID + " exists " + getAccessConfig().getXMLMetadataMgr().exists(objID));
-        if (!getAccessConfig().getXMLMetadataMgr().exists(objID)) {
-            return null;
-        }
-
-        Document objXML = getAccessConfig().getXMLMetadataMgr().retrieveXML(objID);
-        try {
-            String path = "/mycoreobject/metadata/hidden_jpjournalsID/hidden_jpjournalID/text()";
-            if (objID.getTypeId().equals("derivate")) {
-                path = "/mycorederivate/derivate/linkmetas/linkmeta/@xlink:href";
-            }
-            XPath pathToJournalID = XPath.newInstance(path);
-            pathToJournalID.addNamespace(MCRConstants.XLINK_NAMESPACE);
-            Object idTextNode = pathToJournalID.selectSingleNode(objXML);
-
-            if (idTextNode instanceof Text) {
-                return MCRObjectID.getInstance(((Text) idTextNode).getText());
-            } else if (idTextNode instanceof Attribute) {
-                return MCRObjectID.getInstance(((Attribute) idTextNode).getValue());
-            }
-        } catch (JDOMException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
     private boolean isSuperUser() {
@@ -144,5 +107,13 @@ public class AccessStrategy implements MCRAccessCheckStrategy {
 
     private AccessStrategyConfig getAccessConfig() {
         return accessConfig;
+    }
+
+    public void setStrategyChainStart(StrategyStep strategyChainStart) {
+        this.strategyChainStart = strategyChainStart;
+    }
+
+    public StrategyStep getStrategyChainStart() {
+        return strategyChainStart;
     }
 }
