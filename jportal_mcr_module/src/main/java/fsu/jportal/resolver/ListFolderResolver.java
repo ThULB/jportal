14bb1f671e0a8e2895eb94675918156fe64993d7
Huu Chi Vu <chiliego.github@gmail.com>
2012-07-18 12:50:07 +0000

    add list folder resolver

---
+++ jportal_mcr_module/src/main/java/fsu/jportal/resolver/ListFolderResolver.java
@@ -0,0 +1,51 @@
+package fsu.jportal.resolver;
+
+import java.io.File;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.transform.JDOMSource;
+import org.mycore.common.MCRConfiguration;
+
+public class ListFolderResolver implements URIResolver{
+
+    private static final String PREFIX = "templates:";
+    private static final String PROPPREFIX = "prop:";
+
+    @Override
+    public Source resolve(String href, String base) throws TransformerException {
+        href = getPath(href);
+        Element folderList = new Element("folderList");
+        for (File file : new File(href).listFiles()) {
+            String folderName = file.getName();
+            folderList.addContent(createItemElement(folderName));
+        }
+        return new JDOMSource(new Document(folderList));
+    }
+
+    private String getPath(String href) {
+        if(href.startsWith(PREFIX)){
+            href = href.replaceAll(PREFIX, "");
+        }
+        
+        if(href.startsWith(PROPPREFIX)){
+            String property = href.replaceAll(PROPPREFIX, "");
+            href = MCRConfiguration.instance().getString(property);
+        }
+        return href;
+    }
+
+    private Element createItemElement(String folderName) {
+        Element item = new Element("item").setAttribute("value", folderName);
+        Element label = new Element("label").setAttribute("lang", "de", Namespace.XML_NAMESPACE);
+        label.setText(folderName);
+        item.addContent(label);
+        return item;
+    }
+
+}
