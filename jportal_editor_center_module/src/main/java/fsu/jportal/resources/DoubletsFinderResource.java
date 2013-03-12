ca1b4c95eedd4f8153978d3c639f9a52b0a3e1a2
Huu Chi Vu <chiliego.github@gmail.com>
2013-03-12 15:10:45 +0000

    move IP editor into module

---
+++ jportal_editor_center_module/src/main/java/fsu/jportal/resources/DoubletsFinderResource.java
@@ -0,0 +1,28 @@
+package fsu.jportal.resources;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.mycore.common.content.MCRJDOMContent;
+import org.mycore.common.xml.MCRLayoutService;
+
+@Path("doublets")
+public class DoubletsFinderResource {
+    @Context HttpServletRequest request;
+    @Context HttpServletResponse response;
+    
+    @GET
+    public void start() throws IOException, JDOMException{
+        InputStream guiXML = getClass().getResourceAsStream("/doubletsFinder/gui.xml");
+        SAXBuilder saxBuilder = new SAXBuilder();
+        MCRLayoutService.instance().doLayout(request, response, new MCRJDOMContent(saxBuilder.build(guiXML)));
+    }
+}
