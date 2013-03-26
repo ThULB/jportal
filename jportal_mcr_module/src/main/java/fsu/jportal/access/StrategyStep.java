1f5bd1397c3b3a5a74bb5989e02d5c008f57ebe6
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2013-03-26 17:35:02 +0000

    remove old jportal access strategy stuff

--- jportal_mcr_module/src/main/java/fsu/jportal/access/StrategyStep.java
+++
@@ -1,6 +0,0 @@
-package fsu.jportal.access;
-
-public interface StrategyStep {
-    public boolean checkPermission(String id, String permission);
-    public void setAlternative(StrategyStep step);
-}
