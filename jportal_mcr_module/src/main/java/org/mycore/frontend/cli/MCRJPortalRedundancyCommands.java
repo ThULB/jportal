a5174de07a1b373e6abbdc93da5db26eebaa3f54
Huu Chi Vu <chiliego.github@gmail.com>
2013-04-10 15:49:32 +0000

    add fix title after removing doublets

--- jportal_mcr_module/src/main/java/org/mycore/frontend/cli/MCRJPortalRedundancyCommands.java
+++ jportal_mcr_module/src/main/java/org/mycore/frontend/cli/MCRJPortalRedundancyCommands.java
@@ -2,13 +2,21 @@ package org.mycore.frontend.cli;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.mycore.common.content.MCRContent;
+import org.mycore.common.content.transformer.MCRXSLTransformer;
+import org.mycore.common.xsl.MCRParameterCollector;
+import org.mycore.common.xsl.MCRXSLTransformerFactory;
 import org.mycore.datamodel.common.MCRLinkTableManager;
+import org.mycore.datamodel.common.MCRXMLMetadataManager;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.frontend.cli.annotation.MCRCommand;
@@ -26,11 +34,29 @@ public class MCRJPortalRedundancyCommands{
 
     private static final Logger LOGGER = Logger.getLogger(MCRJPortalRedundancyCommands.class);
 
+    @MCRCommand(helpKey = "Deletes and relinks all doublets for a specific type. Doublets signed with doubletOf", syntax = "fix title of {0} for link {1}")
+    public static void removeDoublets(String objId, String linkID){
+        MCRXSLTransformer transformer = new MCRXSLTransformer("/xsl/fixTitleOfLink.xsl");
+        MCRXMLMetadataManager mcrxmlMetadataManager = MCRXMLMetadataManager.instance();
+        MCRObjectID mcrObjId = MCRObjectID.getInstance(objId);
+        
+        try {
+            MCRContent source = mcrxmlMetadataManager.retrieveContent(mcrObjId);
+            MCRParameterCollector parameter = new MCRParameterCollector();
+            parameter.setParameter("linkId", linkID);
+            MCRContent fixedObj = transformer.transform(source, parameter);
+            mcrxmlMetadataManager.update(mcrObjId, fixedObj, new Date());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
     @MCRCommand(helpKey = "Deletes and relinks all doublets for a specific type. Doublets signed with doubletOf", syntax = "jp clean up {0}")
     public static List<String> cleanUp(String type) {
         // get all objects of specific type where doubletOf is not empty
         MCRResults results = getDoubletObjsOfType(type);
-
+        
         ArrayList<String> commandList = new ArrayList<String>();
         Iterator<MCRHit> it = results.iterator();
         while (it.hasNext()) {
@@ -44,7 +70,7 @@ public class MCRJPortalRedundancyCommands{
             }
         }
 
-        commandList.add(new StringBuffer("clean up redundancy in database for type ").append(type).toString());
+//        commandList.add(new StringBuffer("clean up redundancy in database for type ").append(type).toString());
         return commandList;
     }
 
@@ -78,6 +104,8 @@ public class MCRJPortalRedundancyCommands{
             command.append(doublet).append(" ");
             command.append(doubletOf);
             commandList.add(command.toString());
+            commandList.add("fix title of " + source + " for link " + doubletOf);
+            
         }
         // add delete command
         commandList.add(new StringBuffer("delete object ").append(doublet).toString());
