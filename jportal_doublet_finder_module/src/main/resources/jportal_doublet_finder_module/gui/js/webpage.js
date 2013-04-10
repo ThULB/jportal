89a097778eded55de46ac035f1046a3b22af3196
Huu Chi Vu <chiliego.github@gmail.com>
2013-04-10 15:47:31 +0000

    doublet finder finished

--- jportal_doublet_finder_module/src/main/resources/jportal_doublet_finder_module/gui/js/webpage.js
+++ jportal_doublet_finder_module/src/main/resources/jportal_doublet_finder_module/gui/js/webpage.js
@@ -1,18 +1,98 @@
 $(document).ready(function() {
-    $.get("/rsc/search?q=%2BdoubletOf:* %2BobjectType:person",function(data){
-    	$("#numDubPers").text(data.response.numFound);
-    },"json")
-    $.get("/rsc/search?q=%2BdoubletOf:* %2BobjectType:jpinst",function(data){
-    	$("#numDubInst").text(data.response.numFound);
-    },"json")
-    
-    $("#delDubButton").click(function(){
-    	$.ajax({
-    		url: "/rsc/doublets/person",
-    		type: "DELETE",
-    		success: function(){
-    			console.log("del dub type Pers");
-    		}
-    	})
-    })
+	var DubCheckGUI = $("#jportal_doublet_finder_module").on({
+		init : function(){
+			var dubCheckGUI = $(this);
+			
+			$("span.numDub").on("initGUIElem", function(){
+				var numDubDispl = $(this);
+				var type = numDubDispl.attr("type");
+				$.get("/rsc/search?q=%2BdoubletOf:* %2BobjectType:"+type,function(data){
+					var numDoublets = data.response.numFound;
+					numDubDispl.text(numDoublets);
+					if(numDoublets == 0){
+						$("#"+type+"_doublets a.doubletsLink").hide();
+					}else {
+						$("#"+type+"_doublets a.doubletsLink").show();
+						$("#delDubButton").removeAttr("disabled");
+					}
+			    },"json")
+			})
+			
+			$("span.numGND").on("initGUIElem", function(){
+				var numDubDispl = $(this);
+				var type = numDubDispl.attr("type");
+				$.get("/rsc/search?q=-id.gnd:* %2BobjectType:"+type,function(data){
+					var numDoublets = data.response.numFound;
+					numDubDispl.text(numDoublets);
+					if(numDoublets == 0){
+						$("#"+type+"_gnd a.gndLink").hide();
+					}else {
+						$("#"+type+"_gnd a.gndLink").show();
+					}
+				},"json")
+			})
+			
+			$("span.message").on("toggleMsg", function(){
+				$(this).toggle();
+			})
+			
+			$("#delDubButton").on({
+				click : function(){
+					var button = $(this);
+					$.get('/servlets/MCRWebCLIServlet?request=getKnownCommands', function(data){
+						button.trigger("removeDub");
+				    }).error(function(err){
+				        if(err.status == 403){
+				        	$(location).attr('href',"/servlets/MCRLoginServlet?action=login&url=%2Frsc%2Fdoublets");
+				        }
+				    });
+				},
+				
+				removeDub : function(){
+					$.each(["person", "jpinst"], function(index, type){
+						$.get('/servlets/MCRWebCLIServlet?run=jp clean up ' + type, function(data){
+							if(!dubCheckGUI.data("statusTriggered")){
+								dubCheckGUI.trigger("status");
+								dubCheckGUI.data("statusTriggered", true);
+							}
+					    }).error(function(err){
+					        console.log('removeDuplicate failed with Error Code ' + err.status);
+					    });
+					})
+				},
+				
+				initGUIElem : function(){
+					$(this).attr("disabled","disabled");
+				}
+			})
+			
+			$("#delDubButton").trigger("initGUIElem");
+			$("span.numDub").trigger("initGUIElem");
+			$("span.numGND").trigger("initGUIElem");
+		},
+
+		status : function(){
+			var dubCheckGUI = $(this);
+			var processRunning = true;
+			$.ajax({ 
+		        type :  'GET',
+		        url: '/servlets/MCRWebCLIServlet?request=getStatus', 
+		        success: function(data){
+		            processRunning = data.running;
+		        }, 
+		        complete: function(){
+		        	if(processRunning == true){
+		        		$("#progressMsg").show();
+		        		dubCheckGUI.trigger("status");
+		        	}else {
+		        		$("#progressMsg").hide();
+		        		dubCheckGUI.trigger("init");
+		        	}
+		        }, 
+		        timeout: 2000
+		    });
+		}
+	});
+	
+	DubCheckGUI.trigger("init");
 });
