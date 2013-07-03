88ffe7fd5dc6290f0cec899f5231ef78c70e93e9
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2013-07-03 16:14:22 +0000

    fix missing exception due mycore version upgrade

--- jportal_doublet_finder_module/src/main/java/fsu/jportal/resources/DoubletsFinderResource.java
+++ jportal_doublet_finder_module/src/main/java/fsu/jportal/resources/DoubletsFinderResource.java
@@ -3,41 +3,42 @@ package fsu.jportal.resources;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.xml.transform.TransformerException;
 
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-import org.mycore.common.MCRSession;
-import org.mycore.common.MCRSessionMgr;
 import org.mycore.common.content.MCRJDOMContent;
 import org.mycore.common.xml.MCRLayoutService;
 import org.mycore.frontend.cli.MCRKnownCommands;
+import org.xml.sax.SAXException;
 
 @Path("doublets")
 public class DoubletsFinderResource {
-    @Context HttpServletRequest request;
-    @Context HttpServletResponse response;
-    
+
+    @Context
+    HttpServletRequest request;
+
+    @Context
+    HttpServletResponse response;
+
     @GET
-    public void start() throws IOException, JDOMException{
+    public void start() throws IOException, JDOMException, SAXException, TransformerException {
         InputStream guiXML = getClass().getResourceAsStream("/jportal_doublet_finder_module/gui/xml/webpage.xml");
         SAXBuilder saxBuilder = new SAXBuilder();
         MCRLayoutService.instance().doLayout(request, response, new MCRJDOMContent(saxBuilder.build(guiXML)));
     }
-    
+
     @DELETE
     @Path("{type}")
-    public Response removeDuplicatesFor(@PathParam("type") String type){
-        MCRSession session = MCRSessionMgr.getCurrentSession();
+    public Response removeDuplicatesFor(@PathParam("type") String type) {
         MCRKnownCommands mcrKnownCommands = new MCRKnownCommands();
         try {
             mcrKnownCommands.invokeCommand("jp clean up " + type);
@@ -48,4 +49,5 @@ public class DoubletsFinderResource {
             return Response.serverError().build();
         }
     }
+
 }
