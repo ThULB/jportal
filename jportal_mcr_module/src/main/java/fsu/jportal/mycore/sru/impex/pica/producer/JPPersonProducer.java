423a743c449133b61abb666e747ae628c728419c
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2013-07-22 12:42:26 +0000

    fix mononymous person names

--- jportal_mcr_module/src/main/java/fsu/jportal/mycore/sru/impex/pica/producer/JPPersonProducer.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/mycore/sru/impex/pica/producer/JPPersonProducer.java
@@ -36,4 +36,36 @@ public class JPPersonProducer extends PersonProducer {
         return "urmel_class_00000001";
     }
 
+    @Override
+    protected Element createNameContainer(String elementName, String personalName, String collocation, String lastName, String firstName,
+            String nameAffix) {
+        Element nameContainer = generateSubElement(elementName, "de", 0, null, null, null, null);
+        /* family name */
+        if (lastName != null) {
+            if(elementName.equals("alternative")) {
+                nameContainer.setAttribute("type", "complete");
+            }
+            nameContainer.addContent(new Element("lastName").setText(lastName));
+        }
+        /* forename */
+        if (firstName != null) {
+            nameContainer.addContent(new Element("firstName").setText(firstName));
+        }
+        /* add the personal name (if any) */
+        if (personalName != null) {
+            nameContainer.addContent(new Element("name").setText(personalName));
+            if(elementName.equals("alternative")) {
+                nameContainer.setAttribute("type", "single");
+            }
+        }
+        /* and its collocation (if any) */
+        if (collocation != null) {
+            nameContainer.addContent(new Element("collocation").setText(collocation));
+        }
+        if (nameAffix != null) {
+            nameContainer.addContent(new Element("nameAffix").setText(nameAffix));
+        }
+        return nameContainer;
+    }
+
 }
