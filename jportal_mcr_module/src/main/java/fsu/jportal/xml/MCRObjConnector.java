fb9e7f7eeb814f43e3914b574ceb1ffa8dcf6efd
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2013-07-26 08:55:52 +0000

    fix 404 cause null hidden_pubTypes was added to rubrics

--- jportal_mcr_module/src/main/java/fsu/jportal/xml/MCRObjConnector.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/xml/MCRObjConnector.java
@@ -15,6 +15,7 @@ import org.mycore.datamodel.metadata.MCRObjectID;
 
 public class MCRObjConnector {
     private MCRObject mcrObject;
+
     private DataManager dataManager;
 
     public MCRObjConnector(String journalID) {
@@ -38,62 +39,62 @@ public class MCRObjConnector {
             }
         });
     }
-    
+
     public MCRObjConnector(String journalID, DataManager dataManager) {
         this.dataManager = dataManager;
         mcrObject = dataManager.getObj(journalID);
-        
+
     }
 
     public String getRubric() {
         return getRubric("hidden_rubricsID", "hidden_rubricID");
     }
-    
+
     public List<String> getRubrics() {
         List<String> rubricIDs = new ArrayList<>();
-        
+
         for (int i = 1; i <= 6; i++) {
-            String tag = "hidden_classiVol" +i;
-            addRubric(rubricIDs, getRubric(tag,tag));
+            String tag = "hidden_classiVol" + i;
+            addRubric(rubricIDs, getRubric(tag, tag));
         }
-        
-        rubricIDs.add(getRubric("hidden_pubTypesID", "hidden_pubTypeID"));
-        
+
+        addRubric(rubricIDs, getRubric("hidden_pubTypesID", "hidden_pubTypeID"));
+
         for (int i = 1; i <= 4; i++) {
             String mainTag = "hidden_classispub";
             String subTag = "hidden_classipub";
-            
-            if(i != 1){
+
+            if (i != 1) {
                 mainTag = mainTag + i;
                 subTag = subTag + i;
             }
-            
-            addRubric(rubricIDs, getRubric(mainTag,subTag));
+
+            addRubric(rubricIDs, getRubric(mainTag, subTag));
         }
-        
+
         addRubric(rubricIDs, getRubric("hidden_rubricsID", "hidden_rubricID"));
         return rubricIDs;
     }
 
     private void addRubric(List<String> rubricIDs, String rubric) {
-        if(rubric != null) {
+        if (rubric != null) {
             rubricIDs.add(rubric);
         }
     }
-    
+
     private String getRubric(String mainTag, String subTag) {
         MCRMetaElement metadataElement = mcrObject.getMetadata().getMetadataElement(mainTag);
-        if(metadataElement != null){
+        if (metadataElement != null) {
             MCRMetaInterface element = metadataElement.getElementByName(subTag);
-            if(element instanceof MCRMetaLangText){
+            if (element instanceof MCRMetaLangText) {
                 MCRMetaLangText metaLangText = (MCRMetaLangText) element;
                 return metaLangText.getText();
             }
         }
-        
+
         return null;
     }
-    
+
     public void addRubric(MCRCategoryID newRubricID) {
         List<MCRMetaInterface> children = new ArrayList<MCRMetaInterface>();
         MCRMetaLangText elem = new MCRMetaLangText("hidden_rubricID", null, null, 0, null, newRubricID.getRootID());
