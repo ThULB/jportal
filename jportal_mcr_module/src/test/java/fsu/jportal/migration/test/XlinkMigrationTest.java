f04eed4b6866ffe0e54fc0e50d4e1fa1a0d62932
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2014-01-02 08:45:51 +0000

    fix configuration exception

--- jportal_mcr_module/src/test/java/fsu/jportal/migration/test/XlinkMigrationTest.java
+++ jportal_mcr_module/src/test/java/fsu/jportal/migration/test/XlinkMigrationTest.java
@@ -2,7 +2,6 @@ package fsu.jportal.migration.test;
 
 import java.io.InputStream;
 import java.util.HashMap;
-import java.util.Properties;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
@@ -13,18 +12,18 @@ import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 import org.junit.Before;
 import org.junit.Test;
-import org.mycore.common.MCRConfiguration;
+import org.mycore.common.config.MCRConfiguration;
 import org.mycore.common.xml.MCRXSLTransformation;
 
 public class XlinkMigrationTest {
     @Before
     public void init() {
         System.setProperty("MCR.Configuration.File", "config/test.properties");
-        Properties mcrProperties = MCRConfiguration.instance().getProperties();
-        mcrProperties.setProperty("MCR.Metadata.Type.jpjournal", "true");
-        mcrProperties.setProperty("MCR.Metadata.Type.jpvolume", "true");
-        mcrProperties.setProperty("MCR.Metadata.Type.jpinst", "true");
-        mcrProperties.setProperty("MCR.Metadata.Type.person", "true");
+        MCRConfiguration instance = MCRConfiguration.instance();
+        instance.set("MCR.Metadata.Type.jpjournal", "true");
+        instance.set("MCR.Metadata.Type.jpvolume", "true");
+        instance.set("MCR.Metadata.Type.jpinst", "true");
+        instance.set("MCR.Metadata.Type.person", "true");
     }
 
     @Test
@@ -33,7 +32,7 @@ public class XlinkMigrationTest {
         SAXBuilder saxBuilder = new SAXBuilder();
         Document journalXMLDoc = saxBuilder.build(journalXMLStream);
         Source stylesheet = new StreamSource(getClass().getResourceAsStream("/xsl/xlinkMigration.xsl"));
-        Document transform = MCRXSLTransformation.transform(journalXMLDoc, stylesheet, new HashMap());
+        Document transform = MCRXSLTransformation.transform(journalXMLDoc, stylesheet, new HashMap<>());
         
         XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
         xmlOutputter.output(transform, System.out);
