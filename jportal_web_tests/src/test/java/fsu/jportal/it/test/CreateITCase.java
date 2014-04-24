4cd7cb5e4baec55d4adc673f83620a31569ac92a
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2014-04-24 12:59:49 +0000

    add volume and article test

--- jportal_web_tests/src/test/java/fsu/jportal/it/test/CreateITCase.java
+++ jportal_web_tests/src/test/java/fsu/jportal/it/test/CreateITCase.java
@@ -24,14 +24,16 @@ public class CreateITCase extends BaseIntegrationTest {
         DRIVER.findElement(By.name("/mycoreobject/metadata/def.heading/heading/nameAffix")).sendKeys("von");
         DRIVER.findElements(By.name("/mycoreobject/metadata/def.gender/gender/@categid")).get(0).click();
         DRIVER.findElement(By.name("/mycoreobject/metadata/def.dateOfBirth/dateOfBirth")).sendKeys("1749-08-28");
-        DRIVER.findElement(By.name("/mycoreobject/metadata/def.placeOfBirth/placeOfBirth")).sendKeys("Frankurt am Main");
+        DRIVER.findElement(By.name("/mycoreobject/metadata/def.placeOfBirth/placeOfBirth"))
+            .sendKeys("Frankurt am Main");
         DRIVER.findElement(By.name("/mycoreobject/metadata/def.dateOfDeath/dateOfDeath")).sendKeys("1832-03-22");
         DRIVER.findElement(By.name("/mycoreobject/metadata/def.placeOfDeath/placeOfDeath")).sendKeys("Weimar");
         TestUtils.saveForm(DRIVER);
 
         // test page
         TestCase.assertEquals("title does not match", "Goethe, Johann Wolfgang - JPortal", DRIVER.getTitle());
-        TestCase.assertEquals("header does not match", "Goethe, Johann Wolfgang von", DRIVER.findElement(By.id("jp-maintitle")).getText());
+        TestCase.assertEquals("header does not match", "Goethe, Johann Wolfgang von",
+            DRIVER.findElement(By.id("jp-maintitle")).getText());
     }
 
     @Test
@@ -43,7 +45,7 @@ public class CreateITCase extends BaseIntegrationTest {
 
         // fill form
         DRIVER.findElement(By.name("/mycoreobject/metadata/names/name/fullname")).sendKeys(
-                "Thüringer Universitäts- und Landesbibliothek Jena");
+            "Thüringer Universitäts- und Landesbibliothek Jena");
         DRIVER.findElement(By.name("/mycoreobject/metadata/names/name/nickname")).sendKeys("ThULB");
         DRIVER.findElement(By.name("/mycoreobject/metadata/addresses/address/country")).sendKeys("Deutschland");
         DRIVER.findElement(By.name("/mycoreobject/metadata/addresses/address/zipcode")).sendKeys("07743");
@@ -51,17 +53,19 @@ public class CreateITCase extends BaseIntegrationTest {
         DRIVER.findElement(By.name("/mycoreobject/metadata/addresses/address/street")).sendKeys("Bibliotheksplatz");
         DRIVER.findElement(By.name("/mycoreobject/metadata/addresses/address/number")).sendKeys("2");
         DRIVER.findElement(By.name("/mycoreobject/metadata/urls/url/@xlink:title")).sendKeys("ThULB");
-        DRIVER.findElement(By.name("/mycoreobject/metadata/urls/url/@xlink:href")).sendKeys("http://www.thulb.uni-jena.de");
+        DRIVER.findElement(By.name("/mycoreobject/metadata/urls/url/@xlink:href")).sendKeys(
+            "http://www.thulb.uni-jena.de");
         TestUtils.saveForm(DRIVER);
 
         // test page
-        TestCase.assertEquals("title does not match", "Thüringer Universitäts- und Landesbibliothek Jena - JPortal", DRIVER.getTitle());
-        TestCase.assertEquals("header does not match", "Thüringer Universitäts- und Landesbibliothek Jena",
-                DRIVER.findElement(By.id("jp-maintitle")).getText());
+        TestCase.assertEquals("title does not match", "Thüringer Universitäts- und Landesbibliothek Jena - JPortal",
+            DRIVER.getTitle());
+        TestCase.assertEquals("header does not match", "Thüringer Universitäts- und Landesbibliothek Jena", DRIVER
+            .findElement(By.id("jp-maintitle")).getText());
     }
 
     @Test
-    public void createJournal() {
+    public void createJournal() throws Exception {
         TestUtils.home(DRIVER);
         TestUtils.login(DRIVER);
 
@@ -74,7 +78,8 @@ public class CreateITCase extends BaseIntegrationTest {
         // main publisher
         DRIVER.findElement(By.name("_s-sub.select1-/mycoreobject/metadata/participants/participant")).click();
         DRIVER.findElement(By.linkText("Schiller, Friedrich von")).click();
-        WebElement participantSelect = DRIVER.findElement(By.name("/mycoreobject/metadata/participants/participant/@type"));
+        WebElement participantSelect = DRIVER.findElement(By
+            .name("/mycoreobject/metadata/participants/participant/@type"));
         participantSelect.findElement(By.xpath("option[@value='mainPublisher']")).click();
         // from
         WebElement fromSelect = DRIVER.findElement(By.name("/mycoreobject/metadata/dates/date/@type"));
@@ -89,27 +94,55 @@ public class CreateITCase extends BaseIntegrationTest {
         WebElement langSelect = DRIVER.findElement(By.name("/mycoreobject/metadata/languages/language/@categid"));
         langSelect.findElement(By.xpath("option[@value='de']")).click();
         // journal type
-        WebElement journalTypeSelect = DRIVER.findElement(By.name("/mycoreobject/metadata/contentClassis1/contentClassi1/@categid"));
+        WebElement journalTypeSelect = DRIVER.findElement(By
+            .name("/mycoreobject/metadata/contentClassis1/contentClassi1/@categid"));
         journalTypeSelect.findElement(By.xpath("option[@value='historical']")).click();
         // template
-        WebElement templateSelect = DRIVER.findElement(By.name("/mycoreobject/metadata/hidden_templates/hidden_template"));
+        WebElement templateSelect = DRIVER.findElement(By
+            .name("/mycoreobject/metadata/hidden_templates/hidden_template"));
         templateSelect.findElement(By.xpath("option[@value='template_DynamicLayoutTemplates']")).click();
 
         TestUtils.saveForm(DRIVER);
 
         // Tests
-        TestCase.assertEquals("header does not match", "Die Horen", DRIVER.findElement(By.id("jp-maintitle")).getText());
-        DRIVER.findElement(By.linkText("XML")).click();
-        
-//        final String md = "/mycoreobject/metadata/";
-//        TestCase.assertEquals("publisher does not match", "Schiller, Friedrich von",
-//                DRIVER.findElement(By.xpath(md + "participants/participant[@type='mainPublisher']")).getAttribute("xlink:title"));
-//        TestCase.assertEquals("published from does not match", "1795",
-//                DRIVER.findElement(By.xpath(md + "dates/date[@type='published_from']")).getText());
-//        TestCase.assertEquals("published from does not match", "1797",
-//                DRIVER.findElement(By.xpath(md + "dates/date[@type='published_until']")).getText());
-//        TestCase.assertEquals("language from does not match", "de",
-//                DRIVER.findElement(By.xpath(md + "languages/language[@classid='jportal_class_00000004']")).getAttribute("categid"));
+        TestCase
+            .assertEquals("header does not match", "Die Horen", DRIVER.findElement(By.id("jp-maintitle")).getText());
+    }
+
+    @Test
+    public void createVolume() throws Exception {
+        TestUtils.home(DRIVER);
+        TestUtils.login(DRIVER);
+        DRIVER.get(START_URL + "/rsc/editor/jportal_jpjournal_00000001/create/jpvolume");
+
+        // bibl. Beschreibung
+        DRIVER.findElement(By.name("/mycoreobject/metadata/maintitles/maintitle")).sendKeys("Erstausgabe");
+        // Datum
+        DRIVER.findElement(By.name("/mycoreobject/metadata/dates/date")).sendKeys("1947");
+
+        TestUtils.saveForm(DRIVER);
+
+        // Tests
+        TestCase.assertEquals("header does not match", "Erstausgabe", DRIVER.findElement(By.id("jp-maintitle"))
+            .getText());
+    }
+
+    @Test
+    public void createArticle() throws Exception {
+        TestUtils.home(DRIVER);
+        TestUtils.login(DRIVER);
+        DRIVER.get(START_URL + "/rsc/editor/jportal_jpvolume_00000001/create/jparticle");
+
+        // bibl. Beschreibung
+        DRIVER.findElement(By.name("/mycoreobject/metadata/maintitles/maintitle")).sendKeys("Heim ins Frankreich");
+        // Seitenbereich
+        DRIVER.findElement(By.name("/mycoreobject/metadata/sizes/size")).sendKeys("1");
+
+        TestUtils.saveForm(DRIVER);
+
+        // Tests
+        TestCase.assertEquals("header does not match", "Heim ins Frankreich", DRIVER.findElement(By.id("jp-maintitle"))
+            .getText());
     }
 
 }
