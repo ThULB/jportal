372446377040ae1e9d4eb10f5acd5f9ce5c84e6e
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2014-05-16 11:43:08 +0000

    fix warning Editor Resource warning of returning void

--- jportal_mcr_module/src/main/java/fsu/jportal/resources/EditorResource.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/resources/EditorResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 import javax.xml.transform.TransformerException;
 
 import org.jdom2.Document;
@@ -37,16 +38,16 @@ public class EditorResource {
 
     @GET
     @Path("create/{type}")
-    public void create(@PathParam("type") String type) {
-        create(null, type);
+    public Response create(@PathParam("type") String type) {
+        return create(null, type);
     }
 
     @GET
     @Path("{parentID}/create/{type}")
-    public void create(@PathParam("parentID") String parentID, @PathParam("type") String type) {
+    public Response create(@PathParam("parentID") String parentID, @PathParam("type") String type) {
         ParamsXML paramsXML = new ParamsXML();
-        
-        if(parentID != null) {
+
+        if (parentID != null) {
             paramsXML.put("sourceUri", "xslStyle:asParent:mcrobject:" + parentID);
             try {
                 String journalID = new LayoutTools().getJournalID(parentID);
@@ -55,34 +56,34 @@ public class EditorResource {
                 e.printStackTrace();
             }
         }
-        
+
         paramsXML.put("mcrid", "jportal_" + type + "_00000000");
         paramsXML.put("editServlet", "CreateObjectServlet");
         if (cancelURL != null) {
             paramsXML.put("cancelUrl", cancelURL);
         }
-        
+
         initEditor("create", type, paramsXML);
+        return Response.ok().build();
     }
 
     @GET
     @Path("update/{id}")
-    public void update(/*@PathParam("type") String type,*/@PathParam("id") String id) {
+    public Response update(/*@PathParam("type") String type,*/@PathParam("id") String id) {
         String type = MCRObjectID.getIDParts(id)[1];
         ParamsXML paramsXML = new ParamsXML();
         paramsXML.put("mcrid", id);
         paramsXML.put("editServlet", "UpdateObjectServlet");
         paramsXML.put("sourceUri", "xslStyle:mycoreobject-editor:mcrobject:" + id);
         paramsXML.put("cancelUrl", "receive/" + id);
-
         try {
             String journalID = "jpjournal".equals(type) ? id : new LayoutTools().getJournalID(id);
             paramsXML.put("journalID", journalID);
         } catch (TransformerException | JDOMException | IOException | SAXException e) {
             e.printStackTrace();
         }
-        
         initEditor("update", type, paramsXML);
+        return Response.ok().build();
     }
 
     private void initEditor(String method, String type, ParamsXML paramsXML) {
@@ -91,7 +92,7 @@ public class EditorResource {
         paramsXML.put("title", title);
         String validationMsg = MCRTranslation.translateWithBaseName("validationMsg", "editor.i18n.labels");
         paramsXML.put("validationMsg", validationMsg);
-        
+
         String uri = "editorPreProc:start-editor" + paramsXML.getParamStr();
 
         EditorPreProc proc = new EditorPreProc();
@@ -101,6 +102,8 @@ public class EditorResource {
             MCREditorServlet.replaceEditorElements(request, uri, editorJDOM);
             MCRJDOMContent editorReplaced = new MCRJDOMContent(editorJDOM);
             MCRLayoutService.instance().doLayout(request, response, editorReplaced);
+            
+            
         } catch (IOException | SAXException | TransformerException e) {
             e.printStackTrace();
         }
