4d9fc6f0e62577db2f9e54cd618c6dac11134f0f
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2014-07-04 13:32:30 +0000

    -add iview2 urmel logo -fix addscript base url

--- jportal_mcr_module/src/main/java/fsu/jportal/iview2/JPortalIViewConfiguration.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/iview2/JPortalIViewConfiguration.java
@@ -6,6 +6,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import org.mycore.common.MCRSessionMgr;
 import org.mycore.common.MCRSystemUserInformation;
 import org.mycore.common.config.MCRConfiguration;
+import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.iview2.frontend.MCRIViewMetsClientConfiguration;
 
 @XmlRootElement(name = "iviewClientConfiguration")
@@ -14,17 +15,17 @@ public class JPortalIViewConfiguration extends MCRIViewMetsClientConfiguration {
     @Override
     public void setup(HttpServletRequest request) {
         super.setup(request);
-
+        String baseURL = MCRServlet.getBaseURL();
         // piwik
         if (MCRConfiguration.instance().getBoolean("MCR.Piwik.enable", false)) {
-            this.addScript("modules/iview2/js/iview-client-piwik.js");
+            this.addScript(baseURL + "modules/iview2/js/iview-client-piwik.js");
             this.setProperty("MCR.Piwik.baseurl", MCRConfiguration.instance().getString("MCR.Piwik.baseurl"));
             this.setProperty("MCR.Piwik.id", MCRConfiguration.instance().getString("MCR.Piwik.id"));
         }
 
         // derivate link
         if (!MCRSessionMgr.getCurrentSession().getUserInformation().equals(MCRSystemUserInformation.getGuestInstance())) {
-            this.addScript("modules/iview2/js/derivate-link-module.js");
+            this.addScript(baseURL + "modules/iview2/js/derivate-link-module.js");
         }
 
     }
