8c64b6bebd24031ed10862dfec69f542283ecebf
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2014-08-01 10:12:14 +0000

    use new iview strategy

---
+++ jportal_mcr_module/src/main/java/fsu/jportal/iview2/JPortalIViewConfigurationStrategy.java
@@ -0,0 +1,32 @@
+package fsu.jportal.iview2;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.mycore.common.MCRSessionMgr;
+import org.mycore.common.MCRSystemUserInformation;
+import org.mycore.frontend.servlets.MCRServlet;
+import org.mycore.iview2.frontend.configuration.MCRIViewClientConfiguration;
+import org.mycore.iview2.frontend.configuration.MCRIViewClientDefaultConfigurationStrategy;
+
+public class JPortalIViewConfigurationStrategy extends MCRIViewClientDefaultConfigurationStrategy {
+
+    @Override
+    public MCRIViewClientConfiguration get(HttpServletRequest request) {
+        MCRIViewClientConfiguration config = super.get(request);
+
+        String baseURL = MCRServlet.getBaseURL();
+        // css
+        config.addCSS(baseURL + "css/jp-iview.css");
+
+        if (!isPDF(request)) {
+            // derivate link
+            if (!MCRSessionMgr.getCurrentSession().getUserInformation()
+                .equals(MCRSystemUserInformation.getGuestInstance())) {
+                config.addLocalScript("derivate-link-module.js");
+            }
+        }
+
+        return config;
+    }
+
+}
