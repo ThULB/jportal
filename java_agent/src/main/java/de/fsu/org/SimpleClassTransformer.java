1283976d4c3e4b3c9e9f96752351bdacf5233bcc
Huu Chi Vu <chiliego.github@gmail.com>
2014-12-08 07:48:58 +0000

    ASM Fiddle catch ClassLoader getresource

--- java_agent/src/main/java/de/fsu/org/SimpleClassTransformer.java
+++ java_agent/src/main/java/de/fsu/org/SimpleClassTransformer.java
@@ -9,50 +9,78 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
+
+import static org.objectweb.asm.Opcodes.*;
 
 
 public class SimpleClassTransformer implements ClassFileTransformer {
-    static final Logger LOGGER = Logger.getLogger(SimpleClassTransformer.class);
+    public static class MyMethodVisitor extends MethodVisitor {
 
-    static class ClassPrinter extends ClassVisitor{
-        public ClassPrinter(ClassVisitor cv) {
-            super(Opcodes.ASM5, cv);
+        private String className;
+
+        public MyMethodVisitor(MethodVisitor mv, String className) {
+            super(ASM5, mv);
+            this.className = className;
         }
         
+        @Override
+        public void visitCode() {
+            mv.visitVarInsn(ALOAD, 1);
+            mv.visitLdcInsn(".xsl");
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "contains", "(Ljava/lang/CharSequence;)Z", false);
+            Label l1 = new Label();
+            mv.visitJumpInsn(IFEQ, l1);
+            Label l2 = new Label();
+            mv.visitLabel(l2);
+            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
+            mv.visitInsn(DUP);
+            mv.visitLdcInsn("$$$$$$$$$$$$$$$ " + className + " - getResource: ");
+            mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "(Ljava/lang/String;)V", false);
+            mv.visitVarInsn(ALOAD, 1);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+            mv.visitLabel(l1);
+        }
+
+    }
+
+    static final Logger LOGGER = Logger.getLogger(SimpleClassTransformer.class);
+
+    public static class ClassLoaderVisitor extends ClassVisitor{
+        private String className ;
+
+        public ClassLoaderVisitor(ClassWriter cv) {
+            super(ASM5, cv);
+        }
         
         @Override
         public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-            LOGGER.info(name + " extend " + superName + " {");
-        }
-        
-        @Override
-        public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
-            LOGGER.info("    " + desc + " " + name);
-            return null;
+            this.className = name;
+            super.visit(version, access, name, signature, superName, interfaces);
         }
         
         @Override
         public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-            LOGGER.info("    " + name + desc);
-            return null;
-        }
-        
-        @Override
-        public void visitEnd() {
-            LOGGER.info("}");
+            MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+            
+            if(mv != null && "getResource".equals(name)){
+                mv = new MyMethodVisitor(mv, className);
+            }
+            return mv;
         }
     }
     
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-        if("fsu/jportal/resolver/OptionFolderResolver".equals(className)){
-            LOGGER.info("Class : " + className);
+        if(className.contains("ClassLoader")){
             ClassReader classReader = new ClassReader(classfileBuffer);
-            ClassWriter classWriter = new ClassWriter(classReader, 0);
-            ClassPrinter classPrinter = new ClassPrinter(classWriter);
+            ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+            ClassLoaderVisitor classPrinter = new ClassLoaderVisitor(classWriter);
             classReader.accept(classPrinter, 0);
             return classWriter.toByteArray();
         }
