d7db0c691e721fc7665c7b1b8f68b3c7b9898cf3
Huu Chi Vu <chiliego.github@gmail.com>
2015-02-05 13:57:19 +0000

    JP-8 #close fix java.lang.ArrayIndexOutOfBoundsException

--- jportal_mcr_module/src/main/java/fsu/jportal/nio/JarResource.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/nio/JarResource.java
@@ -2,12 +2,14 @@ package fsu.jportal.nio;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +26,17 @@ public class JarResource{
     }
 
     public void init(URL jarURL) throws IOException {
+        String protocol = jarURL.getProtocol();
+        if(protocol.equals("file")){
+            try {
+                this.path = Paths.get(jarURL.toURI());
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+         
+            return;
+        }
+        
         String[] splittedURL = jarURL.toString().split("!");
         String jarFile = splittedURL[0];
         String jarFolder = splittedURL[1];
@@ -46,6 +59,8 @@ public class JarResource{
     }
 
     public void close() throws IOException {
-        zipFS.close();
+        if(zipFS != null) {
+            zipFS.close();
+        }
     }
 }
