60fd8bd2ad5a0e9cfdaef36ca68940c2f78699fe
Huu Chi Vu <chiliego.github@gmail.com>
2015-04-10 16:53:19 +0000

    JP-42 - #close %MCR.basedir% in project folder

--- jportal_mcr_module/src/test/java/fsu/jportal/migration/test/XlinkMigrationTest.java
+++ jportal_mcr_module/src/test/java/fsu/jportal/migration/test/XlinkMigrationTest.java
@@ -1,16 +1,10 @@
 package fsu.jportal.migration.test;
 
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-
 import org.jdom2.Document;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mycore.common.config.MCRConfiguration;
@@ -18,18 +12,67 @@ import org.mycore.common.config.MCRConfigurationLoader;
 import org.mycore.common.config.MCRConfigurationLoaderFactory;
 import org.mycore.common.xml.MCRXSLTransformation;
 
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
 public class XlinkMigrationTest {
+
+    private String mcrBaseDir;
+
     @Before
     public void init() {
         MCRConfiguration mcrConf = MCRConfiguration.instance();
         MCRConfigurationLoader configurationLoader = MCRConfigurationLoaderFactory.getConfigurationLoader();
         Map<String, String> load = configurationLoader.load();
+        load.put("MCR.Metadata.Type.jpjournal", "true");
+        load.put("MCR.Metadata.Type.jpvolume", "true");
+        load.put("MCR.Metadata.Type.jpinst", "true");
+        load.put("MCR.Metadata.Type.person", "true");
+        load.put("MCR.CommandLineInterface.SystemName", "JUnit Test");
+        String buildDir = System.getProperty("project.buildDir");
+        String mcrbaseDirName = "mcrbaseDir";
+
+        try {
+            if (buildDir == null) {
+                mcrBaseDir = Files.createTempDirectory(mcrbaseDirName).toString();
+            } else {
+                mcrBaseDir = Paths.get(buildDir).resolve(mcrbaseDirName).toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Using MCR.basedir: " + mcrBaseDir);
+        load.put("MCR.basedir", mcrBaseDir);
         mcrConf.initialize(load, true);
-        mcrConf.set("MCR.Metadata.Type.jpjournal", "true");
-        mcrConf.set("MCR.Metadata.Type.jpvolume", "true");
-        mcrConf.set("MCR.Metadata.Type.jpinst", "true");
-        mcrConf.set("MCR.Metadata.Type.person", "true");
-        mcrConf.set("MCR.CommandLineInterface.SystemName", "JUnit Test");
+    }
+
+    @After
+    public void cleanUp() {
+        try {
+            Files.walkFileTree(Paths.get(mcrBaseDir), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -39,7 +82,7 @@ public class XlinkMigrationTest {
         Document journalXMLDoc = saxBuilder.build(journalXMLStream);
         Source stylesheet = new StreamSource(getClass().getResourceAsStream("/xsl/xlinkMigration.xsl"));
         Document transform = MCRXSLTransformation.transform(journalXMLDoc, stylesheet, new HashMap<>());
-        
+
         XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
         xmlOutputter.output(transform, System.out);
     }
