d9ee44d107fa359931e32ada651e6e9a20a65b7c
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2015-09-14 11:36:13 +0000

    fix: db session for receiveFile()

--- jportal_mcr_module/src/main/java/fsu/jportal/backend/DerivateTools.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/backend/DerivateTools.java
@@ -64,17 +64,17 @@ import fsu.jportal.util.DerivatePath;
 public class DerivateTools {
     static Logger LOGGER = Logger.getLogger(DerivateTools.class);
 
-    private static boolean cp(final MCRPath source, MCRPath targetDir, String newName, final Map<MCRPath, MCRPath> copyHistory) {
+    private static boolean cp(final MCRPath source, MCRPath targetDir, String newName,
+        final Map<MCRPath, MCRPath> copyHistory) {
         if (newName == null) {
             newName = source.getFileName().toString();
         }
-        
-        if (!Files.isDirectory(targetDir)){
+
+        if (!Files.isDirectory(targetDir)) {
             LOGGER.info("cp: " + targetDir + " is not a directory.");
             return false;
         }
 
-        
         if (Files.exists(targetDir.resolve(newName))) {
             LOGGER.info("cp: " + newName + " allready exists (not copied)");
             return false;
@@ -87,27 +87,28 @@ public class DerivateTools {
                 copyHistory.put(source, MCRPath.toMCRPath(newPath));
             } else {
                 final Path mcrPath = Files.createDirectory(targetDir.resolve(newName));
-                Files.walkFileTree(source, new SimpleFileVisitor<Path>(){
+                Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
                     @Override
                     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                         Path tempPath = mcrPath.resolve(source.relativize(dir));
                         try {
                             Files.copy(dir, tempPath);
                         } catch (FileAlreadyExistsException e) {
-                            if (!Files.isDirectory(tempPath)){
+                            if (!Files.isDirectory(tempPath)) {
                                 throw e;
                             }
                         }
                         copyHistory.put(MCRPath.toMCRPath(dir), MCRPath.toMCRPath(tempPath));
                         return super.preVisitDirectory(dir, attrs);
                     }
+
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                         Path tempPath = mcrPath.resolve(source.relativize(file));
                         Files.copy(file, tempPath, StandardCopyOption.COPY_ATTRIBUTES);
                         copyHistory.put(MCRPath.toMCRPath(file), MCRPath.toMCRPath(tempPath));
                         return super.visitFile(file, attrs);
-                    }                    
+                    }
                 });
             }
             return true;
@@ -125,12 +126,12 @@ public class DerivateTools {
         try {
             String[] sourcePathArray = sourcePath.split(":");
             MCRPath sourceNode = MCRPath.getPath(sourcePathArray[0], sourcePathArray[1]);
-            
+
             if (!Files.exists(sourceNode)) {
                 LOGGER.info("cp: source " + sourcePath + " does not exists (not copied)");
                 return false;
             }
-            
+
             String[] targetPathArray = targetPath.split(":");
             MCRPath targetNode = MCRPath.getPath(targetPathArray[0], targetPathArray[1]);
 
@@ -139,13 +140,13 @@ public class DerivateTools {
                 return false;
             }
             String newFileName = null;
-            if (!Files.isDirectory(targetNode)){
+            if (!Files.isDirectory(targetNode)) {
                 newFileName = targetNode.getFileName().toString();
-            }                    
+            }
 
             Map<MCRPath, MCRPath> copyHistory = new HashMap<MCRPath, MCRPath>();
-            if(cp(sourceNode, targetNode, newFileName, copyHistory)){
-                if (delAfterCopy){
+            if (cp(sourceNode, targetNode, newFileName, copyHistory)) {
+                if (delAfterCopy) {
                     Derivate derivate = new Derivate(sourceNode.getOwner());
                     moveAttachedData(derivate, copyHistory);
                     delete(sourceNode);
@@ -165,9 +166,9 @@ public class DerivateTools {
     private static void moveAttachedData(Derivate srcDerivate, Map<MCRPath, MCRPath> copyHistory) {
         String maindoc = srcDerivate.getMaindoc();
         for (MCRPath sourceNode : copyHistory.keySet()) {
-            MCRPath target = copyHistory.get(sourceNode);            
+            MCRPath target = copyHistory.get(sourceNode);
             String sourcePath = getPathNoLeadingRoot(sourceNode);
-            
+
             List<MCRObjectID> idList = new ArrayList<MCRObjectID>();
             try {
                 idList = DerivateLinkUtil.getLinks(sourceNode);
@@ -178,7 +179,7 @@ public class DerivateTools {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if(sourcePath.equals(maindoc)){
+            if (sourcePath.equals(maindoc)) {
                 Derivate targetDerivate = new Derivate(target.getOwner());
                 targetDerivate.setMaindoc(getPathNoLeadingRoot(target));
             }
@@ -188,8 +189,8 @@ public class DerivateTools {
     }
 
     public static String getPathNoLeadingRoot(MCRPath node) {
-       String nodePath = node.getOwnerRelativePath();
-        if(!node.equals("/")){
+        String nodePath = node.getOwnerRelativePath();
+        if (!node.equals("/")) {
             nodePath = nodePath.substring(1);
         }
         return nodePath;
@@ -220,21 +221,20 @@ public class DerivateTools {
         if (!Files.exists(source)) {
             throw new FileNotFoundException(filePath);
         }
-        
-        if (!Files.isDirectory(source)){
+
+        if (!Files.isDirectory(source)) {
             mvSingleFile(MCRPath.toMCRPath(source), MCRPath.toMCRPath(target));
-        }
-        else{
+        } else {
             final Path mcrPath = Files.createDirectories(target);
             Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
 
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                     Path tempPath = mcrPath.resolve(source.relativize(dir));
-                    try{
+                    try {
                         Files.copy(dir, tempPath);
                     } catch (FileAlreadyExistsException e) {
-                        if (!Files.isDirectory(tempPath)){
+                        if (!Files.isDirectory(tempPath)) {
                             throw e;
                         }
                     }
@@ -257,8 +257,8 @@ public class DerivateTools {
             });
         }
     }
-    
-    private static void mvSingleFile(MCRPath src, MCRPath tgt) throws IOException{
+
+    private static void mvSingleFile(MCRPath src, MCRPath tgt) throws IOException {
         MCRURN urn = URNTools.getURNForFile(src);
         List<MCRObjectID> idList = new ArrayList<MCRObjectID>();
         try {
@@ -267,16 +267,16 @@ public class DerivateTools {
             LOGGER.error("unable to get all file links");
         }
         DerivateLinkUtil.deleteFileLinks(idList, src);
-        
+
         Files.move(src, tgt);
-        
+
         DerivateLinkUtil.setLinks(idList, tgt);
-        if(urn != null){
+        if (urn != null) {
             String path = tgt.getParent().getOwnerRelativePath();
-            if(!path.endsWith("/")){
+            if (!path.endsWith("/")) {
                 path += "/";
             }
-            URNTools.updateURNFileName(urn, path , tgt.getFileName().toString());
+            URNTools.updateURNFileName(urn, path, tgt.getFileName().toString());
         }
     }
 
@@ -287,19 +287,19 @@ public class DerivateTools {
     public static void cp(String sourcePath, String targetPath) {
         cp(sourcePath, targetPath, false);
     }
-    
-    public static boolean mkdir(String path){
+
+    public static boolean mkdir(String path) {
         String[] pathArray = path.split(":");
         MCRPath mcrPath = MCRPath.getPath(pathArray[0], pathArray[1]);
-        
-        if(Files.exists(mcrPath)){
+
+        if (Files.exists(mcrPath)) {
             LOGGER.info("mkdir: " + mcrPath.getFileName().toString() + " allready exists.");
             return false;
         }
-        
+
         MCRPath parentPath = mcrPath.getParent();
-        
-        if(Files.isDirectory(parentPath)){
+
+        if (Files.isDirectory(parentPath)) {
             try {
                 Files.createDirectory(mcrPath);
             } catch (IOException e) {
@@ -312,11 +312,12 @@ public class DerivateTools {
             return false;
         }
     }
-    public static int delete(String derivateID, String path){
+
+    public static int delete(String derivateID, String path) {
         return delete(MCRPath.getPath(derivateID, path));
     }
-    
-    public static int delete(MCRPath mcrPath){
+
+    public static int delete(MCRPath mcrPath) {
         if (!Files.exists(mcrPath)) {
             return 2; //NOT_FOUND
         }
@@ -335,15 +336,16 @@ public class DerivateTools {
             return 1; //OK
         }
         try {
-            Files.walkFileTree(mcrPath, new SimpleFileVisitor<java.nio.file.Path>(){
+            Files.walkFileTree(mcrPath, new SimpleFileVisitor<java.nio.file.Path>() {
 
                 @Override
-                public FileVisitResult visitFile(java.nio.file.Path file, BasicFileAttributes attrs) throws IOException {
+                public FileVisitResult visitFile(java.nio.file.Path file, BasicFileAttributes attrs)
+                    throws IOException {
                     try {
                         DerivateLinkUtil.deleteFileLink(MCRPath.toMCRPath(file));
                     } catch (SolrServerException e) {
                         e.printStackTrace();
-                    }                    
+                    }
                     Files.delete(file);
                     return super.visitFile(file, attrs);
                 }
@@ -353,7 +355,7 @@ public class DerivateTools {
                     Files.delete(dir);
                     return super.postVisitDirectory(dir, exc);
                 }
-                
+
             });
         } catch (IOException e) {
             e.printStackTrace();
@@ -361,26 +363,25 @@ public class DerivateTools {
         }
         return 1; //OK
     }
-    
+
     public static String uploadFile(InputStream inputStream, long filesize, String documentID, String derivateID,
-            String filePath) throws Exception {
+        String filePath) throws Exception {
         MCRSession session = MCRSessionMgr.getCurrentSession();
         if (derivateID == null) {
             String projectID = MCRConfiguration.instance().getString("MCR.SWF.Project.ID", "MCR");
             derivateID = MCRObjectID.getNextFreeId(projectID + '_' + "derivate").toString();
         }
-        MCRUploadHandlerIFS handler;
+        MCRUploadHandlerIFS handler = new MCRUploadHandlerIFS(documentID, derivateID, null);
+        handler.startUpload(1);
         try {
             session.beginTransaction();
-            handler = new MCRUploadHandlerIFS(documentID, derivateID, null);
-            handler.startUpload(1);
+            handler.receiveFile(filePath, inputStream, filesize, null);
             session.commitTransaction();
         } finally {
             session.rollbackTransaction();
+            handler.finishUpload();
+            handler.unregister();
         }
-        handler.receiveFile(filePath, inputStream, filesize, null);
-        handler.finishUpload();
-        handler.unregister();
         return derivateID;
     }
 
@@ -388,8 +389,8 @@ public class DerivateTools {
         Derivate derivate = new Derivate(derivateID);
         derivate.setMaindoc(path);
     }
-    
-    public static JsonObject getChildAsJson(String derivateID, String path, String file){
+
+    public static JsonObject getChildAsJson(String derivateID, String path, String file) {
         Derivate derivate = new Derivate(derivateID);
         MCRDirectory node;
         if (path != null && !"".equals(path.trim())) {
@@ -405,12 +406,11 @@ public class DerivateTools {
             SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
             childJson.addProperty("lastmodified", sdf.format(child.getLastModified().getTime()));
             return childJson;
-        }
-        else {
+        } else {
             return null;
         }
     }
-    
+
     public static JsonObject getDerivateFolderAsJson(String derivateID) {
         Derivate derivate = new Derivate(derivateID);
         MCRDirectory node = derivate.getRootDir();
@@ -428,7 +428,7 @@ public class DerivateTools {
         }
         return jsonObject;
     }
-    
+
     private static JsonArray getFolderChildren(MCRFilesystemNode[] childs) {
         JsonArray jsonArray = new JsonArray();
         for (MCRFilesystemNode child : childs) {
@@ -448,7 +448,7 @@ public class DerivateTools {
         }
         return jsonArray;
     }
-    
+
     public static boolean addURNToDerivate(String derivateID) {
         MCRURNAdder urnAdder = new MCRURNAdder();
         try {
@@ -458,14 +458,13 @@ public class DerivateTools {
             return false;
         }
     }
-    
+
     public static String addURNToFile(String derivatID, String path) {
         MCRURNAdder urnAdder = new MCRURNAdder();
         try {
             if (urnAdder.addURNToSingleFile(derivatID, path)) {
                 return getURNforFile(derivatID, path);
-            }
-            else {
+            } else {
                 return "";
             }
         } catch (Exception e) {
@@ -473,7 +472,7 @@ public class DerivateTools {
             return "";
         }
     }
-    
+
     private static String getURNforFile(String derivate, String path) {
         String fileName = path;
         String pathToFile = "/";
@@ -483,15 +482,15 @@ public class DerivateTools {
         }
         return MCRURNManager.getURNForFile(derivate, pathToFile, fileName);
     }
-    
+
     public static void setLink(String documentID, String path) throws MCRActiveLinkException {
         DerivateLinkUtil.setLink(MCRJerseyUtil.getID(documentID), path);
     }
-    
+
     public static void removeLink(String documentID, String path) throws MCRActiveLinkException {
         DerivateLinkUtil.removeLink(MCRJerseyUtil.getID(documentID), path);
     }
-    
+
     public static JsonObject getDerivateAsJson(String derivateID, String path) {
         MCRJSONManager gsonManager = MCRJSONManager.instance();
         gsonManager.registerAdapter(new DerivateTypeAdapter());
@@ -516,20 +515,20 @@ public class DerivateTools {
 
         return json;
     }
-    
+
     private static boolean isHidden(String derivateID) {
         MCRDerivate derObj = MCRMetadataManager.retrieveMCRDerivate(MCRObjectID.getInstance(derivateID));
         Document xml = derObj.createXML();
         XPathFactory xpF = XPathFactory.instance();
-        XPathExpression<Element> xpE = xpF.compile("mycorederivate/derivate",Filters.element());
+        XPathExpression<Element> xpE = xpF.compile("mycorederivate/derivate", Filters.element());
         Element derivateNode = (Element) xpE.evaluateFirst(xml);
         Attribute displayAttr = derivateNode.getAttribute("display");
-        if (displayAttr != null){
+        if (displayAttr != null) {
             return Boolean.parseBoolean(displayAttr.getValue());
         }
         return false;
     }
-    
+
     public static boolean tileDerivate(String derivateID) {
         if (!MCRIView2Tools.isDerivateSupported(derivateID)) {
             LOGGER.info("Skipping tiling of derivate " + derivateID + " as it's main file is not supported by IView2.");
@@ -541,9 +540,9 @@ public class DerivateTools {
             LOGGER.info("Derivate " + derivateID + " does not exist or is not a directory!");
             return false;
         }
-        
+
         try {
-            Files.walkFileTree(derivateRoot, new SimpleFileVisitor<java.nio.file.Path>(){
+            Files.walkFileTree(derivateRoot, new SimpleFileVisitor<java.nio.file.Path>() {
 
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
@@ -554,19 +553,19 @@ public class DerivateTools {
                     }
                     return FileVisitResult.CONTINUE;
                 }
-                
+
             });
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-       return true;
+        return true;
     }
 
     public static String getMD5forFile(String derivateID, String path) {
         Derivate derivate = new Derivate(derivateID);
         MCRFile mcrFile = (MCRFile) derivate.getChildByPath(path);
-        if (mcrFile != null ){
+        if (mcrFile != null) {
             return mcrFile.getMD5();
         }
         return "";
@@ -576,8 +575,7 @@ public class DerivateTools {
         String urnObjects = "";
         try {
             urnObjects = MCRConfiguration.instance().getString("MCR.URN.Enabled.Objects");
-        }
-        catch (MCRConfigurationException e) {
+        } catch (MCRConfigurationException e) {
             LOGGER.info("Property MCR.URN.Enabled.Object not set, URN allocation not possible.");
         }
         if (urnObjects.contains("derivate")) {
