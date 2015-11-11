037734993029fa298c8efebaa34962569c502b55
Huu Chi Vu <chiliego.github@gmail.com>
2015-11-11 12:02:17 +0000

    JP-73 - #comment refactoring Selenium Webtests ACL Editor IT - add IT Test data module

---
+++ jportal_IT-TestData/src/main/java/fsu/jportal/handler/it/TestDataHandler.java
@@ -0,0 +1,32 @@
+package fsu.jportal.handler.it;
+
+import fsu.jportal.handler.MCRInitHandler;
+import org.mycore.common.events.MCRStartupHandler;
+
+import javax.servlet.ServletContext;
+
+/**
+ * Created by chi on 09.11.15.
+ * @author Huu Chi Vu
+ */
+public class TestDataHandler extends MCRInitHandler {
+    @Override
+    public String getName() {
+        return "IT - Init test data";
+    }
+
+    @Override
+    public int getPriority() {
+        return 1;
+    }
+
+    @Override
+    protected void run() {
+
+    }
+
+    @Override
+    protected void runWithSession() {
+        createACLRules("Creating IT ACL rules...", "/config/jportal_IT-TestData/acl/IT-ACLRules-commands");
+    }
+}
