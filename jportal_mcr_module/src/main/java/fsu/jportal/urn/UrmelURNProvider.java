53aed4567104fcfe56c69bf970ab1189febfcc70
Huu Chi Vu <chiliego.github@gmail.com>
2015-11-26 17:13:26 +0000

    JP-76 - #comment Create URN doesn't work - refactoring UrmelURNProvider copy from de.uni_jena.thulb.archive.urn.UrmelURNProvider

--- jportal_mcr_module/src/main/java/fsu/jportal/urn/UrmelURNProvider.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/urn/UrmelURNProvider.java
@@ -2,42 +2,118 @@ package fsu.jportal.urn;
 
 import java.util.UUID;
 
-import org.mycore.urn.services.MCRAbstractURNProvider;
+import org.mycore.common.config.MCRConfiguration;
+import org.mycore.urn.services.MCRIURNProvider;
 import org.mycore.urn.services.MCRURN;
 
 /**
- * @author shermann
+ * @author chi
  */
-public class UrmelURNProvider extends MCRAbstractURNProvider {
-    private static final String URMEL = "urmel";
+public class UrmelURNProvider implements MCRIURNProvider {
+    public static final String NISS;
 
-    /** Generates a single URN */
-    public MCRURN generateURN() {
-        return new MCRURN(MCRURN.getDefaultNamespaceIdentifiers(), UrmelURNProvider.URMEL + "-" + UrmelURNProvider.getUUID().toString());
+    static {
+        NISS = MCRConfiguration.instance().getString("MCR.URN.NISS", "urmel");
     }
 
-    /**
-     * Generates multiple urns
-     * 
-     * @param int the amount of urn to generate, must be &gt;= 1
-     */
+    public MCRURN generateURN() {
+        return new MCRURN(MCRURN.getDefaultNamespaceIdentifiers(),
+                UrmelURNProvider.NISS + "-" + UrmelURNProvider.getUUID().toString());
+    }
+
     public MCRURN[] generateURN(int amount) {
+        NSSpecificPartPattern generator = new NSSpecificPartPattern() {
+            @Override
+            public String generate(int i) {
+                return UrmelURNProvider.NISS + "-" + UrmelURNProvider.getUUID().toString() + "-" + i;
+            }
+        };
+
+        return generateURN(amount, MCRURN.getDefaultNamespaceIdentifiers(), generator);
+    }
+
+    @Override
+    public MCRURN[] generateURN(int amount, MCRURN base) {
+        if (base == null) {
+            return null;
+        }
+
+        PatternGenerator generator = new PatternGenerator(amount, base.getNamespaceIdentfiersSpecificPart());
+
+        return generateURN(amount, base.getNamespaceIdentfiers(), generator);
+    }
+
+    public MCRURN[] generateURN(int amount, MCRURN base, String setId) {
+        int parsedInt = Integer.parseInt(setId);
+
+        if (parsedInt < 0) {
+            throw new IllegalArgumentException(
+                    "setId must represent an integer >= 0, e.g. 1, 001 or 00004, but was " + setId);
+        }
+
+        if (base == null || setId == null) {
+            return null;
+        }
+
+        if (amount == 1) {
+            return new MCRURN[] { generateURN(base, setId) };
+        }
+
+        PatternGenerator generator = new PatternGenerator(amount, base.getNamespaceIdentfiersSpecificPart());
+        generator.setId(setId);
+
+        return generateURN(amount, base.getNamespaceIdentfiers(), generator);
+    }
+
+    public String getNISS() {
+        return UrmelURNProvider.NISS;
+    }
+
+    public MCRURN generateURN(MCRURN base, String setId) {
+        return new MCRURN(base.getNamespaceIdentfiers(), base.getNamespaceIdentfiersSpecificPart() + "-" + setId);
+    }
+
+    public MCRURN[] generateURN(int amount, String[] nsIdentifiers, NSSpecificPartPattern pattern) {
         if (amount < 1) {
             return null;
         }
 
         MCRURN[] urn = new MCRURN[amount];
 
-        for (int i = 1; i <= amount; i++) {
-            urn[i - 1] = new MCRURN(MCRURN.getDefaultNamespaceIdentifiers(), UrmelURNProvider.URMEL + "-"
-                    + UrmelURNProvider.getUUID().toString() + "-" + i);
+        for (int i = 0; i < amount; i++) {
+            urn[i] = new MCRURN(nsIdentifiers, pattern.generate(i + 1));
         }
-        return urn;
+        return null;
+    }
+
+    private class PatternGenerator implements NSSpecificPartPattern {
+        private final String format;
+
+        private final String nsIdentifiersSpecParts;
+
+        private String id = "";
+
+        public PatternGenerator(int leadingZeros, String nsIdentifiersSpecParts) {
+            this.format = "%0" + leadingZeros + "d";
+            this.nsIdentifiersSpecParts = nsIdentifiersSpecParts;
+        }
+
+        public void setId(String id) {
+            this.id = id + "-";
+        }
+
+        @Override
+        public String generate(int i) {
+            return nsIdentifiersSpecParts + "-" + id + String.format(format, i);
+        }
+    }
+
+    interface NSSpecificPartPattern {
+        public String generate(int i);
     }
 
     /** Central method to generate uuids */
     private static UUID getUUID() {
         return UUID.randomUUID();
     }
-
 }
