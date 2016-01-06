b3434ebc252b8f19f1a55b98761b6839b03ed5af
Huu Chi Vu <chiliego.github@gmail.com>
2016-01-06 15:21:11 +0000

    refactoring using Optionals instead of if/null

--- jportal_mcr_module/src/main/java/fsu/jportal/backend/JPInstitution.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/backend/JPInstitution.java
@@ -1,17 +1,16 @@
 package fsu.jportal.backend;
 
-import java.util.Optional;
-import java.util.stream.StreamSupport;
-
-import org.mycore.datamodel.metadata.MCRMetaElement;
 import org.mycore.datamodel.metadata.MCRMetaInstitutionName;
 import org.mycore.datamodel.metadata.MCRMetaLangText;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
 /**
  * Abstraction of a jportal institution. Be aware that this class is not fully implemented.
- * 
+ *
  * @author Matthias Eichner
  */
 public class JPInstitution extends JPLegalEntity {
@@ -28,7 +27,7 @@ public class JPInstitution extends JPLegalEntity {
 
     /**
      * Creates a new JPPerson container for the given mcrId.
-     * 
+     *
      * @param mcrId a mycore object id
      */
     public JPInstitution(MCRObjectID mcrId) {
@@ -37,7 +36,7 @@ public class JPInstitution extends JPLegalEntity {
 
     /**
      * Creates a new JPPerson container for the given mycore object.
-     * 
+     *
      * @param mcrObject the mycore object
      */
     public JPInstitution(MCRObject mcrObject) {
@@ -51,7 +50,7 @@ public class JPInstitution extends JPLegalEntity {
 
     /**
      * Returns the name of the institution.
-     * 
+     *
      * @return name of the person
      */
     @Override
@@ -61,23 +60,24 @@ public class JPInstitution extends JPLegalEntity {
 
     /**
      * Returns the full name of the institution.
-     * 
+     *
      * @return name of the institution.
      */
     protected String getInstitutionName() {
-        MCRMetaElement names = object.getMetadata().getMetadataElement("names");
-        if (names == null) {
-            return null;
-        }
-        Optional<MCRMetaInstitutionName> name = StreamSupport.stream(names.spliterator(), false)
-            .filter(m -> m.getInherited() == 0).map(c -> (MCRMetaInstitutionName) c).findFirst();
-        return name.orElse(null).getFullName();
+        return Optional.ofNullable(object.getMetadata().getMetadataElement("names"))
+                       .map(_names -> StreamSupport.stream(_names.spliterator(), false))
+                       .flatMap(
+                               stream -> stream.filter(_names -> _names.getInherited() == 0)
+                                               .map(c -> (MCRMetaInstitutionName) c)
+                                               .findFirst()
+                       ).orElse(null).getFullName();
     }
 
     @Override
     public Optional<String> getId(String type) {
         return metadataStreamNotInherited("identifiers", MCRMetaLangText.class).filter(t -> t.getType().equals(type))
-            .map(MCRMetaLangText::getText).findFirst();
+                                                                               .map(MCRMetaLangText::getText)
+                                                                               .findFirst();
     }
 
 }
