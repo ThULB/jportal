cd640cccad1238ece663820f4c89dfcc80f7675d
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2016-05-04 12:46:06 +0000

    use new MCRSolrSearchUtils.stream() method for mets generation

--- jportal_mcr_module/src/main/java/fsu/jportal/mets/JPortalMetsGenerator.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/mets/JPortalMetsGenerator.java
@@ -1,31 +1,26 @@
 package fsu.jportal.mets;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.*;
-
-import fsu.jportal.frontend.SolrToc;
-import org.apache.log4j.Logger;
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.params.ModifiableSolrParams;
-import org.mycore.datamodel.metadata.*;
-import org.mycore.datamodel.niofs.MCRPath;
-import org.mycore.mets.model.MCRMETSHierarchyGenerator;
-import org.mycore.mets.model.files.FileSec;
-import org.mycore.mets.model.struct.*;
-import org.mycore.solr.MCRSolrClientFactory;
-import org.mycore.solr.search.MCRSolrSearchUtils;
-import org.mycore.solr.search.MCRSolrURL;
-
 import static fsu.jportal.frontend.SolrToc.buildQuery;
 import static fsu.jportal.frontend.SolrToc.getSort;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.mycore.datamodel.metadata.MCRMetaElement;
+import org.mycore.datamodel.metadata.MCRMetaInterface;
+import org.mycore.datamodel.metadata.MCRMetaLangText;
+import org.mycore.datamodel.metadata.MCRMetaLinkID;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.datamodel.metadata.MCRObjectID;
+import org.mycore.mets.model.MCRMETSHierarchyGenerator;
+import org.mycore.solr.MCRSolrClientFactory;
+import org.mycore.solr.search.MCRSolrSearchUtils;
+
 public class JPortalMetsGenerator extends MCRMETSHierarchyGenerator {
-    private static Logger LOGGER = Logger.getLogger(JPortalMetsGenerator.class);
 
     protected String getType(MCRObject obj) {
         return obj.getId().getTypeId().substring(2);
@@ -59,40 +54,17 @@ public class JPortalMetsGenerator extends MCRMETSHierarchyGenerator {
         String parentID = parentObject.getId().toString();
         String sort = getSort(parentID, objectType);
         ModifiableSolrParams solrParams = buildQuery(parentID, objectType, sort);
+        solrParams.set("fl", "id objectType");
         SolrClient solrClient = MCRSolrClientFactory.getSolrClient();
 
-        SolrTocHandler tocHandler = new SolrTocHandler(parentObject);
-        try {
-            return MCRSolrSearchUtils.list(solrClient, solrParams, tocHandler);
-        } catch (SolrServerException | IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private class SolrTocHandler implements MCRSolrSearchUtils.DocumentHandler<MCRMetaLinkID> {
-        private final MCRObject parentObject;
-
-        public SolrTocHandler(MCRObject parentObject) {
-            this.parentObject = parentObject;
-        }
-
-        @Override
-        public MCRMetaLinkID getResult(SolrDocument document) {
-            String id = (String) document.getFieldValue("id");
+        return MCRSolrSearchUtils.stream(solrClient, solrParams).map(doc -> {
+            String id = (String) doc.getFieldValue("id");
             String label = parentObject.getLabel();
             if (label == null || label.equals("")) {
                 label = id;
             }
-
             return new MCRMetaLinkID("child", MCRObjectID.getInstance(id), label, id);
-        }
-
-        @Override
-        public String fl() {
-            return "id objectType";
-        }
+        }).collect(Collectors.toList());
     }
 
     protected String getEnclosingDerivateLinkName() {
