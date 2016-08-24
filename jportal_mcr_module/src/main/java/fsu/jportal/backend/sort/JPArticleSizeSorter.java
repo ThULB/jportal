d4956b178741350bd90a36bb5189d0278e6446c5
Huu Chi Vu <chiliego.github@gmail.com>
2016-08-24 14:39:04 +0000

    fix for sizes with special character "*" "[]"

--- jportal_mcr_module/src/main/java/fsu/jportal/backend/sort/JPArticleSizeSorter.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/backend/sort/JPArticleSizeSorter.java
@@ -1,13 +1,13 @@
 package fsu.jportal.backend.sort;
 
-import java.util.Comparator;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import fsu.jportal.backend.JPArticle;
 import fsu.jportal.backend.JPPeriodicalComponent;
 import fsu.jportal.util.JPComponentUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Default implementation to sort jparticle's by their size.
@@ -29,13 +29,32 @@ public class JPArticleSizeSorter implements JPSorter {
             if (size1 == null || size2 == null) {
                 return 0;
             }
+
+            int specialCharCompareResult = compareSpecialChar(size1, size2);
+            if (specialCharCompareResult != 0) {
+                return specialCharCompareResult * getOrderSign(order);
+            }
+
             size1 = size1.replaceAll("[^0-9]", "");
             size2 = size2.replaceAll("[^0-9]", "");
             int intSize1 = getIntSize(child1, size1);
             int intSize2 = getIntSize(child2, size2);
-            return Integer.compare(intSize1, intSize2) * (order.equals(Order.ASCENDING) ? 1 : -1);
+            return Integer.compare(intSize1, intSize2) * getOrderSign(order);
 
         };
+    }
+
+    private int getOrderSign(Order order) {
+        return order.equals(Order.ASCENDING) ? 1 : -1;
+    }
+
+    private int compareSpecialChar(String size1, String size2) {
+        return Arrays.stream(new String[] { "*", "[" })
+                        .mapToInt(c -> getSign(c) * Boolean.compare(size1.startsWith(c), size2.startsWith(c))).sum();
+    }
+
+    private int getSign(String c) {
+        return "*".equals(c)? -1 : 1;
     }
 
     private int getIntSize(JPPeriodicalComponent child, String size) {
