eb4526165e09b661140fdc4fec2050c9a5f61290
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2016-09-15 14:14:19 +0000

    update to jersey2

--- jportal_mcr_module/src/main/java/fsu/jportal/resources/ResourceAccess.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/resources/ResourceAccess.java
@@ -1,30 +1,34 @@
 package fsu.jportal.resources;
 
+import javax.ws.rs.container.ContainerRequestContext;
+
 import org.mycore.access.MCRAccessManager;
 import org.mycore.frontend.jersey.filter.access.MCRResourceAccessChecker;
 
-import com.sun.jersey.spi.container.ContainerRequest;
-
-public class ResourceAccess implements MCRResourceAccessChecker{
+public class ResourceAccess implements MCRResourceAccessChecker {
 
     private static final String RESOURCE_EDIT = "resourceEdit";
+
     private static final String DELETE_DOUBLETS = "delete-doublets";
+
     private static final String PATH_DOUBLETS = "doublets";
+
     private static final String PATH_DEFAULT = "default";
 
     @Override
-    public boolean isPermitted(ContainerRequest request) {
+    public boolean isPermitted(ContainerRequestContext request) {
         String method = request.getMethod();
-        String path = request.getPath();
-        
-        if("GET".equals(method) && !MCRAccessManager.hasRule(path, RESOURCE_EDIT)){
+        String path = request.getUriInfo().getPath();
+
+        if ("GET".equals(method) && !MCRAccessManager.hasRule(path, RESOURCE_EDIT)) {
             return true;
         }
 
-        if("DELETE".equals(method) && path.contains(PATH_DOUBLETS) && MCRAccessManager.checkPermission(PATH_DEFAULT, DELETE_DOUBLETS)){
+        if ("DELETE".equals(method) && path.contains(PATH_DOUBLETS)
+            && MCRAccessManager.checkPermission(PATH_DEFAULT, DELETE_DOUBLETS)) {
             return true;
         }
-        
+
         return MCRAccessManager.checkPermission(path, RESOURCE_EDIT);
     }
 
