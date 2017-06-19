5f41515e21e5b5851542888160aa45265e3efa24
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2017-06-19 15:22:14 +0000

    improve jpinst gnd/sru import

--- jportal_mcr_module/src/main/java/fsu/jportal/backend/pica/JPInstitutionProducer.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/backend/pica/JPInstitutionProducer.java
@@ -1,8 +1,10 @@
 package fsu.jportal.backend.pica;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.jdom2.Element;
+import org.jdom2.Namespace;
 import org.mycore.datamodel.metadata.MCRMetaInstitutionName;
 import org.mycore.datamodel.metadata.MCRMetaLangText;
 import org.mycore.datamodel.metadata.MCRMetaXML;
@@ -15,6 +17,52 @@ public class JPInstitutionProducer extends CorporationProducer {
 
     public JPInstitutionProducer() {
         super("jpinst");
+    }
+
+    @Override
+    protected Element createMetadataElement() throws IOException {
+        Element metadata = new Element("metadata");
+        metadata.setAttribute("lang", "de", Namespace.XML_NAMESPACE);
+        Element meta;
+
+        if ((meta = createDefHeadingElement("029A")) != null) {
+            metadata.addContent(meta);
+        }
+
+        if ((meta = createDefAlternativeElement("029@")) != null) {
+            metadata.addContent(meta);
+        }
+
+        // if standard fields 029A and 029@ are not available, we try 065A (due to migration to gnd format)
+        // this may need further analysis :s
+        if (meta == null) {
+            if ((meta = createDefHeadingElement("065A")) != null) {
+                metadata.addContent(meta);
+            }
+        }
+
+        if ((meta = createDefIdentifierElement()) != null) {
+            metadata.addContent(meta);
+        }
+
+        if ((meta = createTimesOfActivity()) != null) {
+            metadata.addContent(meta);
+        }
+
+        if ((meta = createPlacesOfActivity()) != null) {
+            metadata.addContent(meta);
+        }
+
+/*
+        if ((meta = createDefRoleElement()) != null) {
+            metadata.addContent(meta);
+        }
+*/
+        if ((meta = createNotesElement()) != null) {
+            metadata.addContent(meta);
+        }
+
+        return metadata;
     }
 
     protected Element createDefHeadingElement(String tag) {
@@ -51,9 +99,8 @@ public class JPInstitutionProducer extends CorporationProducer {
 
     protected void createMetaXMLNameElement(Element defElement, String elementName, String fieldName) {
         List<Datafield> datafields = this.record.getDatafieldsByName(fieldName);
-        List<Subfield> subfields;
-        for (int i = 0; i < datafields.size(); i++) {
-            subfields = datafields.get(i).getSubfieldsByCode("a");
+        for(Datafield datafield : datafields) {
+            List<Subfield> subfields = datafield.getSubfieldsByCode("a");
             if (!subfields.isEmpty()) {
                 Element childElement = new Element(elementName).setAttribute("inherited", "0");
                 Element nameElement = new Element("name").setText(subfields.get(0).getValue());
@@ -70,9 +117,28 @@ public class JPInstitutionProducer extends CorporationProducer {
         return generateDefElement("notes", MCRMetaLangText.class, true, false);
     }
 
-    protected Element createGndLinkElement() {
-        // TODO: there is no gndlink entry in jpinst datamodel
-        return null;
+    protected Element createTimesOfActivity() {
+        String start = this.record.getValue("060R", "a", "4", "datb");
+        String end = this.record.getValue("060R", "b", "4", "datb");
+        if (start == null) {
+            return null;
+        }
+        String value = start + (end != null ? " - " + end : "");
+        Element timesOfActivity = generateDefElement("timesOfActivity", MCRMetaLangText.class, false, true);
+        Element timeOfActivity = generateSubElement("timeOfActivity", null, 0, null, "plain", null, null).setText(value);
+        timesOfActivity.addContent(timeOfActivity);
+        return timesOfActivity;
+    }
+
+    protected Element createPlacesOfActivity() {
+        String name = this.record.getValue("065R", "a", "4", "orta");
+        if (name == null) {
+            return null;
+        }
+        Element placesOfActivity = generateDefElement("placesOfActivity", MCRMetaLangText.class, false, true);
+        Element placeOfActivity = generateSubElement("placeOfActivity", null, 0, null, "plain", null, null).setText(name);
+        placesOfActivity.addContent(placeOfActivity);
+        return placesOfActivity;
     }
 
 }
