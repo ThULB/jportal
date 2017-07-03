d6404ae72a5eaf24910f31934f89a3e8c5b8ce91
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2017-07-03 17:17:46 +0000

    -update MetsHandler -dont use MCRUpdateMetsOnDerivateChangeEventHandler anymore

--- jportal_mcr_module/src/main/java/fsu/jportal/backend/event/UpdateMetsHandler.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/backend/event/UpdateMetsHandler.java
@@ -1,30 +1,37 @@
 package fsu.jportal.backend.event;
 
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import fsu.jportal.mets.MetsAutoGenerator;
+import fsu.jportal.util.DerivateLinkUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.mycore.common.events.MCREvent;
 import org.mycore.common.events.MCREventHandlerBase;
 import org.mycore.common.inject.MCRInjectorConfig;
+import org.mycore.datamodel.common.MCRMarkManager;
 import org.mycore.datamodel.metadata.MCRDerivate;
 import org.mycore.datamodel.metadata.MCRMetaLinkID;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.datamodel.metadata.MCRObjectUtils;
 import org.mycore.datamodel.niofs.MCRPath;
+import org.mycore.mets.events.MCRUpdateMetsOnDerivateChangeEventHandler;
+import org.mycore.mets.tools.MCRMetsSave;
 
-import fsu.jportal.mets.MetsAutoGenerator;
-import fsu.jportal.util.DerivateLinkUtil;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Checks if it is required to rebuild the corresponding mets.xml. Uses
  * the {@link MetsAutoGenerator} to do so.
- * 
+ *
  * @author Matthias Eichner
  */
 public class UpdateMetsHandler extends MCREventHandlerBase {
+
+    private static final Logger LOGGER = LogManager.getLogger(UpdateMetsHandler.class);
 
     private MetsAutoGenerator metsAutoGenerator;
 
@@ -34,20 +41,12 @@ public class UpdateMetsHandler extends MCREventHandlerBase {
 
     @Override
     protected void handleObjectUpdated(MCREvent evt, MCRObject obj) {
-        MCRObjectID mcrId = obj.getId();
-        String type = mcrId.getTypeId();
-        // apply only for journal, volume and article
-        if (!(type.equals("jpjournal") || type.equals("jparticle") || type.equals("jpvolume"))) {
-            return;
-        }
+        handleObject(obj);
+    }
 
-        // fetch all derivates
-        List<String> derivateLinks = getDerivateLinks(obj);
-
-        // run through derivates
-        for (String derivateID : derivateLinks) {
-            metsAutoGenerator.add(MCRObjectID.getInstance(derivateID));
-        }
+    @Override
+    protected void handleObjectRepaired(MCREvent evt, MCRObject obj) {
+        handleObject(obj);
     }
 
     @Override
@@ -56,33 +55,77 @@ public class UpdateMetsHandler extends MCREventHandlerBase {
     }
 
     @Override
+    protected void handleDerivateUpdated(MCREvent evt, MCRDerivate der) {
+        handleDerivate(der);
+    }
+
+    @Override
     protected void handlePathDeleted(MCREvent evt, Path path, BasicFileAttributes attrs) {
-        handleMetsFile(path);
+        handlePath(path);
     }
 
     @Override
     protected void handlePathCreated(MCREvent evt, Path path, BasicFileAttributes attrs) {
-        handleMetsFile(path);
+        handlePath(path);
+    }
+
+    @Override
+    protected void handlePathRepaired(MCREvent evt, Path path, BasicFileAttributes attrs) {
+        handlePath(path);
+    }
+
+    @Override
+    protected void handlePathUpdated(MCREvent evt, Path path, BasicFileAttributes attrs) {
+        handlePath(path);
+    }
+
+    private void handleObject(MCRObject object) {
+        MCRObjectID mcrId = object.getId();
+        String type = mcrId.getTypeId();
+        // apply only for journal, volume and article
+        if (!(type.equals("jpjournal") || type.equals("jparticle") || type.equals("jpvolume"))) {
+            return;
+        }
+
+        // fetch all derivates
+        List<String> derivateLinks = getDerivateLinks(object);
+
+        // run through derivates
+        for (String derivateID : derivateLinks) {
+            MCRObjectID mcrDerivateID = MCRObjectID.getInstance(derivateID);
+            if (MCRMarkManager.instance().isMarkedForDeletion(mcrDerivateID)) {
+                continue;
+            }
+            metsAutoGenerator.add(mcrDerivateID);
+        }
+    }
+
+    private void handleDerivate(MCRDerivate derivate) {
+        MCRMetsSave.updateMetsOnUrnGenerate(derivate);
     }
 
     /**
-     * Removes the derivate from mets creation if the mets.xml is created
-     * or removed.
-     * 
-     * @param path the path
+     * Updates the mets.xml if a file change appear. If the mets.xml itself was
+     * changed, then we remove the derivate from auto generation, cause we expect
+     * the right mets.xml in the first place.
+     *
+     * @param path the path which is updated/repaired/removed
      */
-    private void handleMetsFile(Path path) {
+    private void handlePath(Path path) {
         MCRPath mcrPath = MCRPath.toMCRPath(path);
         String fileName = mcrPath.getFileName().toString();
+        MCRObjectID derivateId = MCRObjectID.getInstance(mcrPath.getOwner());
         if ("mets.xml".equals(fileName)) {
-            metsAutoGenerator.remove(MCRObjectID.getInstance(mcrPath.getOwner()));
+            metsAutoGenerator.remove(derivateId);
+        } else {
+            metsAutoGenerator.add(derivateId);
         }
     }
 
     /**
      * Returns a list of derivates which are in relation to the given object. This includes
      * all derivates of the structure part and all derivate links.
-     * 
+     *
      * @param obj mycore object
      * @return list of derivate id's
      */
@@ -95,11 +138,8 @@ public class UpdateMetsHandler extends MCREventHandlerBase {
         // run through the derivates of ancestors and self
         List<MCRObject> ancestorsAndSelf = MCRObjectUtils.getAncestorsAndSelf(obj);
         // return list of all derivate Links
-        return ancestorsAndSelf.stream()
-                               .flatMap(o -> o.getStructure().getDerivates().stream())
-                               .map(MCRMetaLinkID::getXLinkHref)
-                               .distinct()
-                               .collect(Collectors.toList());
+        return ancestorsAndSelf.stream().flatMap(o -> o.getStructure().getDerivates().stream())
+            .map(MCRMetaLinkID::getXLinkHref).distinct().collect(Collectors.toList());
     }
 
 }
