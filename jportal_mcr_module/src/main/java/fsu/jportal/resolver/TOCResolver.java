83d86c23bb86f24f1e2827f211cb650b7120e3b1
Matthias Eichner <matthias.eichner@thulb.uni-jena.de>
2018-05-14 16:05:29 +0000

    improve exception handling in TOC resolver

--- jportal_mcr_module/src/main/java/fsu/jportal/resolver/TOCResolver.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/resolver/TOCResolver.java
@@ -6,6 +6,7 @@ import javax.xml.transform.URIResolver;
 import fsu.jportal.backend.JPObjectType;
 import fsu.jportal.frontend.toc.JPTocResults;
 import fsu.jportal.xml.JPXMLFunctions;
+import org.apache.logging.log4j.LogManager;
 import org.jdom2.Element;
 import org.jdom2.transform.JDOMSource;
 import org.mycore.datamodel.metadata.MCRObjectID;
@@ -15,28 +16,32 @@ import org.mycore.datamodel.metadata.MCRObjectID;
  * @author Huu Chi Vu
  */
 @URIResolverSchema(schema = "toc")
-public class SolrTOCResolver implements URIResolver {
+public class TOCResolver implements URIResolver {
 
     /*
      * toc:parentID:objectType:rows:start
      */
     @Override
     public Source resolve(String href, String base) {
-        String[] uriParts = href.split(":");
-        if (uriParts.length < 5) {
-            throw new IllegalArgumentException("Invalid format of uri given to resolve: " + href);
+        try {
+            String[] uriParts = href.split(":");
+            if (uriParts.length < 5) {
+                throw new IllegalArgumentException("Invalid format of uri given to resolve: " + href);
+            }
+            String parentID = uriParts[1];
+            String objectType = uriParts[2];
+            int rows = Integer.valueOf(uriParts[3]);
+            int start = getStart(href, parentID, objectType, rows, uriParts[4]);
+
+            MCRObjectID objectId = MCRObjectID.getInstance(parentID);
+            JPObjectType type = JPObjectType.valueOf(objectType);
+            JPTocResults tocResult = new JPTocResults(objectId, type, start, rows);
+            Element resultsElement = tocResult.toXML();
+            return new JDOMSource(resultsElement);
+        } catch (Exception exc) {
+            LogManager.getLogger().error("Unable to resolve " + href, exc);
+            return new JDOMSource(new Element("results"));
         }
-        String parentID = uriParts[1];
-        String objectType = uriParts[2];
-        int rows = Integer.valueOf(uriParts[3]);
-        int start = getStart(href, parentID, objectType, rows, uriParts[4]);
-
-        MCRObjectID objectId = MCRObjectID.getInstance(parentID);
-        JPObjectType type = JPObjectType.valueOf(objectType);
-        JPTocResults tocResult = new JPTocResults(objectId, type, start, rows);
-        Element resultsElement = tocResult.toXML();
-
-        return new JDOMSource(resultsElement);
     }
 
     private int getStart(String href, String parentID, String objectType, int rows, String startStr) {
