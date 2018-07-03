63097258cf2b0dea8b824bc195918d26b1f6e2f9
Huu Chi Vu <chiliego.github@gmail.com>
2018-07-03 14:48:04 +0000

    pdf creator use proxy instead of wrackDM17

--- jportal_mcr_module/src/main/java/fsu/jportal/resources/ProxyResource.java
+++ jportal_mcr_module/src/main/java/fsu/jportal/resources/ProxyResource.java
@@ -7,51 +7,56 @@ import org.mycore.common.config.MCRConfiguration;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.function.Function;
 
 @Path("proxy")
-public class LogoResource {
-    private static Logger LOGGER = LogManager.getLogger(LogoResource.class);
+public class ProxyResource {
+    private static Logger LOGGER = LogManager.getLogger(ProxyResource.class);
 
     @GET
     @Path("logo/{path:.*}")
     public Response get(@PathParam("path") String path) {
-        String urlString = getLogoURL();
-        if (urlString == null) {
-            return Response.serverError().build();
-        } else {
-            urlString = urlString + "/" + path;
-        }
-        return resolveURL(urlString);
+        return resolveURL(getProperty("JP.Site.Logo.url"), url -> url + "/" + path);
     }
 
-    private Response resolveURL(String urlString) {
+    @GET
+    @Path("pdf{path:.*}")
+    public Response getPDFCreatorURL(@PathParam("path") String path, @Context UriInfo info) {
+        Function<String,String> urlModifier = url -> {
+            String query = info.getRequestUri().getQuery();
+            String p = !path.equals("") ? "/" + path : path;
+            return url + p + "?" + query;
+        };
+
+        return resolveURL(getProperty("JP.Viewer.PDFCreatorURI"), urlModifier);
+    }
+
+    private Response resolveURL(String urlString, Function<String,String> urlModifier) {
+        if (urlString == null) {
+            return Response.serverError().build();
+        }
+
+        if(urlModifier != null) {
+            urlString = urlModifier.apply(urlString);
+        }
+
         try {
             URL url = new URL(urlString);
             URLConnection connection = url.openConnection();
             connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
-            String contentType = connection.getContentType();
 
-            return Response.ok(reader, contentType).build();
+            return Response.ok(connection.getContent(), connection.getContentType()).build();
         } catch (IOException e) {
             LOGGER.error("Could not load the URL: " + urlString + " ---> error: " + e.getMessage());
         }
         return Response.serverError().build();
-    }
-
-    @GET
-    @Path("logoURLBase")
-    public String getLogoURL() {
-        return getProperty("JP.Site.Logo.url");
-    }
-
-    public String getPDFCreatorURL() {
-        return getProperty("MCR.Viewer.PDFCreatorURI");
     }
 
     private String getProperty(String property) {
